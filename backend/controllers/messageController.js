8b91c31f33afe96b20b661b36d08d522b80ad6a8
Pinto <pintokevin2002@hotmail.com>
2024-09-16 11:47:59 +0200

    Adding the addMessage function.

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,10 +1,41 @@
 const asyncHandler = require("express-async-handler");
-
+const Message = require('../models/messageModel');
+const User = require('../models/userModel');
 
 const getMessages = asyncHandler(async (req, res) => {
     res.status(200).json({'message': 'lol'});
 });
 
+const addMessage= asyncHandler(async (req, res) => {
+    const { text } = req.body;
+    const user = req.user._id;
+
+    if(!text || text === ''){
+        res.status(400);
+        throw new Error("Please fill all the required fields");
+    }
+
+    const message = await Message.create({
+        text,
+        user
+    });
+
+    const userRelated = await User.findById(user);
+    userRelated.messages.push(message);
+    await userRelated.save();
+
+    if(message){
+        res.status(201).json({
+            _id: message._id,
+            text: message.text
+        });
+    }else{
+        res.status(400);
+        throw new Error("An error occur while attempting to create the user. Please retry later.");
+    }
+});
+
 module.exports = {
     getMessages,
+    addMessage,
 }
--- backend/routes/messageRoute.js
+++ backend/routes/messageRoute.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const messageController = require('../controllers/messageController');
+const {protect} = require("../middleware/authMiddleware");
 
 // @route Message route (GET)
 // @desc Route to get all message
 // @access Public
 router.route('/').get(messageController.getMessages);
+
+router.route('/').post(protect, messageController.addMessage);
 
 module.exports = router;
