f8e90f5d037d6e7834ab2fd7b2cd495f84daed16
Pinto <pintokevin2002@hotmail.com>
2024-09-16 11:46:46 +0200

    Correcting the auth id.

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -9,7 +9,8 @@ const protect = asyncHandler(async (req, res, next) => {
     if(token){
         try {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            req.user = await User.findById(decoded.id).select('-password');
+            req.user = await User.findById(decoded.userId).select('-password');
+            console.log(decoded.useId);
             next();
         }catch(e){
             console.log(e);
