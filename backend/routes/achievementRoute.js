f20827dc0592200e51abf6ac1e98ef72fa1be603
Kevin <pintokevin2002@hotmail.com>
2024-09-18 16:46:24 +0200

    Adding the protect function.

--- backend/routes/achievementRoute.js
+++ backend/routes/achievementRoute.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const achievementController = require('../controllers/achievementController');
+const {protect} = require('../middleware/authMiddleware');
 
 // @route User route (POST)
 // @desc Route to create an achievement
 // @access Private
-router.route('/').post(achievementController.createAchievement);
+router.route('/').post(protect, achievementController.createAchievement);
 
 // @route User route (PUT)
 // @desc Route to update an achievement
 // @access Private
-router.route('/:_id').put(achievementController.updateAchievement);
+router.route('/:_id').put(protect, achievementController.updateAchievement);
 
 module.exports = router;
