1501d6b5adff588940681403a03b7d909eb1f2e9
Kevin <pintokevin2002@hotmail.com>
2024-09-18 14:47:40 +0200

    Adding the model for the achievements.

---
+++ backend/models/achievementModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const achievementSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Achievement', achievementSchema);
