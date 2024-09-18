ca08aac1ca6b3b26a544eae66d1abde142da6f10
Kevin <pintokevin2002@hotmail.com>
2024-09-18 15:44:25 +0200

    Adding a minlength for the username.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,7 +5,8 @@ const userSchema = mongoose.Schema({
     username: {
         type: String,
         required: true,
-        trim: true
+        trim: true,
+        minlength: 3
     },
     email: {
         type: String,
