76da9cb080c7ff037142829c517da1715cd1c461
Kevin <pintokevin2002@hotmail.com>
2024-09-20 15:52:39 +0200

    Adding the addComment function

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -14,6 +14,13 @@ const addComment = asyncHandler(async (req, res) => {
         user: req.user._id,
         message: req.params.messageId
     });
+
+    if(comment){
+        res.status(201).json(comment);
+    }else{
+        res.status(400);
+        throw new Error("An error occur while attempting to create the comment. Please retry later.");
+    }
 });
 
 module.exports = {
