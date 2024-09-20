4d0774164b190a7c22550ebd4079c508adbb24ca
Kevin <pintokevin2002@hotmail.com>
2024-09-20 15:56:34 +0200

    Updating comment

--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const commentController = require("../controllers/commentController");
 const {protect} = require("../middleware/authMiddleware");
 
-// @route Message route (POST)
+// @route Comment route (POST)
 // @desc Route to add a comment
 // @access Private
 router.route('/:messageId').post(protect, commentController.addComment);
