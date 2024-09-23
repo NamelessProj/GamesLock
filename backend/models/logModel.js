fc1a615a0973dacf15403a2ff1be96f3de484e17
Kevin <pintokevin2002@hotmail.com>
2024-09-23 19:23:45 +0200

    Adding the logModel.js

---
+++ backend/models/logModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const logModel = mongoose.Schema({
+    country: {
+        type: String,
+        required: true,
+        default: '',
+        trim: true
+    },
+    region: {
+        type: String,
+        required: true,
+        default: '',
+        trim: true
+    },
+    system: {
+        type: String,
+        required: true,
+        default: '',
+        trim: true
+    },
+    systemName: {
+        type: String,
+        required: true,
+        default: '',
+        trim: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model('Log', logModel);
