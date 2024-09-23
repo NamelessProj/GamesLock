a137a041f0af1c67f0a1461887b0a4a7c3e68d0f
Kevin <pintokevin2002@hotmail.com>
2024-09-23 15:38:33 +0200

    Adding the route to get a comment with an id

--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const commentController = require("../controllers/commentController");
 const {protect, adminProtect} = require("../middleware/authMiddleware");
 
+// @route Comment route (GET)
+// @desc Route to get a comment
+// @access Public
+router.route('/:_id').get(commentController.getCommentById);
+
 // @route Comment route (POST)
 // @desc Route to add a comment
 // @access Private
