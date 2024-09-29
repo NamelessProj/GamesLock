bb7bcac76dcb412af77941e18e00b4fca5a2d13c
Kevin <pintokevin2002@hotmail.com>
2024-09-29 21:56:08 +0200

    Adding followRoutes.js

---
+++ backend/routes/followRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const FollowController = require('../controllers/followController');
+const {protect} = require('../middleware/authMiddleware');
+
+// @route Follow route (POST)
+// @desc Route to create a follow relationship between two accounts
+// @access Private
+router.route('/:_id').post(protect, FollowController.addFollow);
+
+// @route Follow route (DELETE)
+// @desc Route to delete a follow relationship between two accounts
+// @access Private
+router.route('/:_id').delete(protect, FollowController.deletingFollow);
+
+module.exports = router;
