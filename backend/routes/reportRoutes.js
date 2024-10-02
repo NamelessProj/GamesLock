164804a2018f09a7da30a025a9a10da8da0e79a5
Pinto <pintokevin2002@hotmail.com>
2024-10-02 13:53:28 +0200

    Updating reportRoutes.js to use the new getAllReports method

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const reportController = require('../controllers/reportController');
-const {protect} = require('../middleware/authMiddleware');
+const {protect, adminProtect} = require('../middleware/authMiddleware');
+
+// @route Report route (GET)
+// @desc Route to get all reports
+// @access Private (admin)
+router.route('/').get(adminProtect, reportController.getAllReports);
 
 // @route Report route (POST)
 // @desc Route to add a report for a message
