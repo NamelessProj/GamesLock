bb83f4c8b409f831af97815db1e88796ac2568de
Pinto <pintokevin2002@hotmail.com>
2024-10-02 11:52:06 +0200

    Updating messageController.js to change the error message of the addMessage method

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -134,7 +134,7 @@ const addMessage= asyncHandler(async (req, res) => {
         res.status(201).json({message, newAchievement});
     }else{
         res.status(400);
-        throw new Error("An error occur while attempting to create the user. Please retry later.");
+        throw new Error("An error occur while attempting to post the message. Please retry later.");
     }
 });
 
