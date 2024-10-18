fb5d61a0bd4de6cfd3be2dcd1f8fdbf8a81d71da
Kevin <pintokevin2002@hotmail.com>
2024-10-18 17:08:35 +0200

    Updating Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,36 @@
-import Posts from "../components/Posts.jsx";
+import Posts from "../components/Posts/Posts.jsx";
 
 const Home = () => {
 
-    const posts = [];
+    const posts = [
+        {
+            id: 3,
+            user: {
+                id: 1,
+                username: 'user1'
+            },
+            body: 'Hello everyone, nice to meet you!',
+            createdAt: '2024-10-18T15:30:23.254+02:00'
+        },
+        {
+            id: 2,
+            user: {
+                id: 2,
+                username: 'UserName'
+            },
+            body: 'I\'m here to stay.',
+            createdAt: '2024-10-18T12:11:33.254+02:00'
+        },
+        {
+            id: 1,
+            user: {
+                id: 1,
+                username: 'user1'
+            },
+            body: 'This is the body of post 1.',
+            createdAt: '2024-09-16T11:21:54.254+00:00'
+        }
+    ];
 
     return (
         <main>
