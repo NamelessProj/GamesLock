60d9bf241f7fe5a600107ad5506ced18b24ef316
Kevin <pintokevin2002@hotmail.com>
2024-10-18 17:08:01 +0200

    Adding a route for the achievement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import {Routes, Route} from "react-router-dom";
 import Layout from "./components/Layout.jsx";
 import Home from "./pages/Home.jsx";
 import ErrorPage from "./pages/ErrorPage.jsx";
+import AchievementsPage from "./pages/AchievementsPage/AchievementsPage.jsx";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
         <Routes>
             <Route path="/" element={<Layout />}>
                 <Route index element={<Home />} />
+                <Route path="achievements" element={<AchievementsPage />} />
                 <Route path="*" element={<ErrorPage />} />
             </Route>
         </Routes>
