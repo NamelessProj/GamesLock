7d90b22f0cb0e311ad32c41c501c669fdb1b12cd
Kevin <pintokevin2002@hotmail.com>
2024-10-20 14:43:39 +0200

    Adding SVG

---
+++ frontend/src/components/SVG/SvgBell.jsx
@@ -0,0 +1,12 @@
+import './svg.css';
+
+const SvgBell = ({label="Notifications"}) => {
+    return (
+        <svg className="defaultSvg" width="346" height="432" viewBox="0 0 346 432" fill="none" xmlns="http://www.w3.org/2000/svg" aria-label={label}>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M346 173C346 77.4547 268.545 0 173 0C77.4548 0 0 77.4547 0 173V365H346V173ZM333.677 173.677C333.677 84.9374 261.739 13 173 13C84.2607 13 12.3233 84.9374 12.3233 173.677V352H333.677V173.677ZM173.5 420C152.706 420 135.17 406.051 129.739 387H217.262C211.83 406.051 194.294 420 173.5 420Z"/>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M173 0C268.545 0 346 77.4547 346 173V365H0V173C0 77.4547 77.4547 0 173 0ZM173.497 432C148.535 432 128.268 411.899 128 387H218.995C218.727 411.899 198.459 432 173.497 432Z"/>
+        </svg>
+    );
+};
+
+export default SvgBell;
---
+++ frontend/src/components/SVG/SvgComment.jsx
@@ -0,0 +1,12 @@
+import './svg.css';
+
+const SvgComment = ({label="Comment"}) => {
+    return (
+        <svg className="defaultSvg" width="204" height="132" viewBox="0 0 204 132" fill="none" xmlns="http://www.w3.org/2000/svg" aria-label={label}>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M0 10C0 4.47715 4.47705 0 10 0H194C199.523 0 204 4.47715 204 10V94C204 99.5229 199.523 104 194 104H47.8354L14.7083 131.474L9.69995 103.996C4.31592 103.837 0 99.4225 0 94V10ZM5 10C5 7.23858 7.23858 5 10 5H193C195.761 5 198 7.23858 198 10V94C198 96.7614 195.761 99 193 99H47.9093L18.2663 123.584L13.7855 99H10C7.23858 99 5 96.7614 5 94V10Z"/>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M10 0C4.47715 0 0 4.47715 0 10V94C0 99.4225 4.31586 103.837 9.69987 103.996L14.7082 131.474L47.8354 104H194C199.523 104 204 99.5229 204 94V10C204 4.47715 199.523 0 194 0H10Z"/>
+        </svg>
+    );
+};
+
+export default SvgComment;
