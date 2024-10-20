6c53c2dc2dc76ace5243d00f25e06e364690bbf1
Kevin <pintokevin2002@hotmail.com>
2024-10-20 18:01:01 +0200

    Adding a class to the title of the error page

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 const ErrorPage = () => {
     return (
         <main className="page-not-found">
-            <h1>404</h1>
+            <h1 className="error-title">404</h1>
             <h2>Page not found</h2>
             <p>The resource you've been searching doesn't seem to exist.</p>
             <p>
