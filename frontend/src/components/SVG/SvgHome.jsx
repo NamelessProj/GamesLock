77dfa57a1881f630832f6852bb46ed66696603b2
Kevin <pintokevin2002@hotmail.com>
2024-10-20 15:06:10 +0200

    Adding a nav boolean

--- frontend/src/components/SVG/SvgHome.jsx
+++ frontend/src/components/SVG/SvgHome.jsx
@@ -1,8 +1,8 @@
 import './svg.css';
 
-const SvgHome = ({label="Home"}) => {
+const SvgHome = ({label="Home", nav=false}) => {
     return (
-        <svg className="defaultSvg" width="104" height="129" viewBox="0 0 104 129" fill="none" xmlns="http://www.w3.org/2000/svg" aria-label={label}>
+        <svg className={(nav ? 'navSvg ' : '') + "defaultSvg"} width="104" height="129" viewBox="0 0 104 129" fill="none" xmlns="http://www.w3.org/2000/svg" aria-label={label}>
             <path fill-rule="evenodd" clip-rule="evenodd" d="M51.5285 0L103.057 42H103.026V129H66.0286V81H37.0286V129H0.0259628V42H0L51.5285 0ZM51.5286 6L98.2638 44.093H98.0285V125H69.0285V78H34.0285V125H5.02855V44.093H4.79346L51.5286 6Z"/>
             <path fill-rule="evenodd" clip-rule="evenodd" d="M103.057 42L51.5285 0L0 42H0.0259628V129H37.0286V81H66.0286V129H103.026V42H103.057Z"/>
         </svg>
--- frontend/src/components/SVG/SvgShare.jsx
+++ frontend/src/components/SVG/SvgShare.jsx
@@ -1,8 +1,8 @@
 import './svg.css';
 
-const SvgShare = ({label="Share"}) => {
+const SvgShare = ({label="Share", nav=false}) => {
     return (
-        <svg className="defaultSvg" width="217" height="174" viewBox="0 0 217 174" fill="none" xmlns="http://www.w3.org/2000/svg" aria-label={label}>
+        <svg className={(nav ? 'navSvg ' : '') + "defaultSvg"} width="217" height="174" viewBox="0 0 217 174" fill="none" xmlns="http://www.w3.org/2000/svg" aria-label={label}>
             <path fill-rule="evenodd" clip-rule="evenodd" d="M141.25 155.019L217 77.5093L141.25 0V41.0093H79H68H0L94 173.509L79.2479 114.009H141.25V155.019ZM146.828 141.773L209.313 77.8365L146.828 13.9002V49.0093H75.1659H69H10L87.5392 158.307L75.3167 109.009H146.828V141.773Z"/>
             <path fill-rule="evenodd" clip-rule="evenodd" d="M217 77.5093L141.25 155.019V114.009H79.2479L94 173.509L0 41.0093H68H79H141.25V0L217 77.5093Z"/>
         </svg>
