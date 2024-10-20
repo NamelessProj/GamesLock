39cd5fdc294e975a0717aa04cfcba0936ea4e2f6
Kevin <pintokevin2002@hotmail.com>
2024-10-20 15:30:55 +0200

    Putting the logo in the header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,15 +1,17 @@
 import './header.css';
 import {Link} from "react-router-dom";
+import SvgGear from "../SVG/SvgGear.jsx";
+import SvgLogo from "../SVG/SvgLogo.jsx";
 
 const Header = () => {
     return (
         <header className="app_header">
             <Link to={'/'}>
-                <p>Logo</p>
+                <SvgLogo />
             </Link>
 
             <Link to={'/settings'}>
-                <p>Settings</p>
+                <SvgGear />
             </Link>
         </header>
     );
