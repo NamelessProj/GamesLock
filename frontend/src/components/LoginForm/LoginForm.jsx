95ea52bee7d7f81045f6b9a0ec9dac7f6d2421ac
Kevin <pintokevin2002@hotmail.com>
2024-10-23 17:53:47 +0200

    Updating the login form

--- frontend/src/components/LoginForm/LoginForm.jsx
+++ frontend/src/components/LoginForm/LoginForm.jsx
@@ -1,12 +1,104 @@
-import LoginInputs from "../LoginInputs/LoginInputs.jsx";
+import {useEffect, useState} from "react";
+import {useUserStore} from "../../stores/userStore.js";
+import {useAuthStore} from "../../stores/authStore.js";
+import {useNavigate} from "react-router-dom";
+import {Alert, Button, Card, CardBody, CardFooter, Input, Spinner, Typography} from "@material-tailwind/react";
 
 const LoginForm = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const {user, error, userLoading, login, success} = useUserStore();
+    const {setCredentials, userInfo} = useAuthStore();
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(success){
+            setCredentials({user});
+            navigate('/');
+        }
+    }, [navigate, success]);
+
+    useEffect(() => {
+        if(userInfo){
+            navigate('/');
+        }
+    }, [userInfo]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            await login({email, password});
+        }catch(e){
+            console.log(e);
+        }
+    }
+
     return (
-        <form>
-            <h1>Login</h1>
-            <LoginInputs />
-            <button type="submit">Login</button>
-        </form>
+        <>
+            <div className="flex justify-center my-6">
+                {error && (
+                    <Alert color="red" className="w-1/2">{error}</Alert>
+                )}
+            </div>
+
+            <section className="my-12 flex justify-center">
+                {userLoading ? (
+                    <Spinner className="h-12 w-12" color="orange" />
+                ):(
+                    <Card className="w-96" role="form">
+                        <CardBody className="flex flex-col gap-8">
+                            <Typography variant="h1" className="devgothic important">
+                                Login
+                            </Typography>
+
+                            <Input
+                                type="email"
+                                inputMode="email"
+                                label="Email"
+                                name="email"
+                                size="lg"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <Input
+                                type="password"
+                                label="Password"
+                                name="password"
+                                size="lg"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </CardBody>
+                        <CardFooter className="pt-0">
+                            <Button
+                                variant="gradient"
+                                fullWidth
+                                color="orange"
+                                onClick={handleSubmit}
+                            >
+                                Login
+                            </Button>
+
+                            <Typography variant="small" className="mt-6 flex justify-center">
+                                Don't have an account?
+                                <Typography
+                                    as="a"
+                                    href="/register"
+                                    variant="small"
+                                    color="orange"
+                                    className="ml-1 font-bold"
+                                >
+                                    Register
+                                </Typography>
+                            </Typography>
+                        </CardFooter>
+                    </Card>
+                )}
+            </section>
+        </>
     );
 };
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,39 @@
+import {useUserStore} from "../stores/userStore.js";
+import {useAuthStore} from "../stores/authStore.js";
+import {useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
+
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const {user, error, userLoading, login, success} = useUserStore;
+    const {setCredentials, userInfo} = useAuthStore;
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(success){
+            setCredentials({user});
+            navigate('/');
+        }
+    }, [navigate, success]);
+
+    useEffect(() => {
+        if(userInfo){
+            navigate('/');
+        }
+    }, [userInfo]);
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try{
+            await login({email, password});
+        }catch(e){
+            console.log(e);
+        }
+    }
+
     return (
         <main className="login_form">
 
