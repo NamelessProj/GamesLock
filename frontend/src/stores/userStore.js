fbd3be08cb659b02e1559472195c48c328cc1b6d
Kevin <pintokevin2002@hotmail.com>
2024-10-23 17:52:41 +0200

    Creation of the stores

---
+++ frontend/src/stores/userStore.js
@@ -0,0 +1,40 @@
+import {create} from "zustand";
+import axios from "axios";
+
+export const useUserStore = create((set) => ({
+    user: null,
+    userLoading: false,
+    error: null,
+    success: false,
+    message: null,
+
+    register: async (data) => {
+        set({userLoading: true, error: null});
+        try{
+            const response = await axios.post('http://localhost:3000/api/user/register', data);
+            set(() => ({user: response.data, userLoading: false, success: true}));
+        }catch(error){
+            set({error: error.message, userLoading: false});
+        }
+    },
+
+    login: async (data) => {
+        set({userLoading: true, error: null});
+        try{
+            const response = await axios.post('http://localhost:3000/api/user/login', data);
+            set(() => ({user: response.data, userLoading: false, success: true}));
+        }catch(error){
+            set({error: error.message, userLoading: false});
+        }
+    },
+
+    userLogout: async () => {
+        set({userLoading: true, error: null});
+        try{
+            const response = await axios.post('http://localhost:3000/api/user/logout');
+            set(() => ({user: response.data, userLoading: false}));
+        }catch(error){
+            set({error: error.message, userLoading: false});
+        }
+    }
+}));
