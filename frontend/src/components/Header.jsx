ed3b07d7b6de9f6d552249e43042b0d297016418
Kevin <pintokevin2002@hotmail.com>
2024-10-24 14:54:41 +0200

    Moving Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,10 @@
-import './header.css';
 import {Link} from "react-router-dom";
-import SvgGear from "../SVG/SvgGear.jsx";
-import SvgLogo from "../SVG/SvgLogo.jsx";
+import SvgGear from "./SVG/SvgGear.jsx";
+import SvgLogo from "./SVG/SvgLogo.jsx";
 
 const Header = () => {
     return (
-        <header className="app_header">
+        <header className="flex justify-between items-center py-4 px-8">
             <Link to={'/'}>
                 <SvgLogo className="fill-primary-400 w-10 h-auto" />
             </Link>
