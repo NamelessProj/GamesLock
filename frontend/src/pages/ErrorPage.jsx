040591592af966b714300f1b4e5432c528967bed
Kevin <pintokevin2002@hotmail.com>
2024-10-24 16:43:27 +0200

    Updating the 404 page

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -1,14 +1,24 @@
 import {Link} from "react-router-dom";
+import GradientText from "../components/GradientText.jsx";
+import {Typography} from "@material-tailwind/react";
 
 const ErrorPage = () => {
     return (
         <main className="page-not-found">
-            <h1 className="error-title">404</h1>
-            <h2>Page not found</h2>
-            <p>The resource you've been searching doesn't seem to exist.</p>
-            <p>
-                <Link to={'/'}>Go back to the home page.</Link>
-            </p>
+            <GradientText colors={['#bc4b27', '#fddc75', '#bc4b27', '#fddc75']}>
+                <Typography as="h1" className="font-dev text-7xl">
+                    404
+                </Typography>
+            </GradientText>
+            <Typography as="h2" className="text-center text-xl font-bold">
+                Page Not Found
+            </Typography>
+            <Typography className="text-center mx-auto">
+                The resource you've been searching doesn't seem to exist.
+            </Typography>
+            <Typography className="text-center mx-auto">
+                <Link to={'/'} className="text-primary-400 font-bold">Go back to the home page.</Link>
+            </Typography>
         </main>
     );
 };
