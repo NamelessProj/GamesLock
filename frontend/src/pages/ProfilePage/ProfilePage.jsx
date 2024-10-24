388495ed5b550de5b01216b854e7c1ddee84c957
Kevin <pintokevin2002@hotmail.com>
2024-10-24 18:57:32 +0200

    Updating ProfilePage.jsx

--- frontend/src/pages/ProfilePage/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage/ProfilePage.jsx
@@ -3,17 +3,13 @@ import {format} from "date-fns";
 import ExperienceBar from "../../components/ExperienceBar/ExperienceBar.jsx";
 import Posts from "../../components/Posts/Posts.jsx";
 import CountUp from "../../components/CountUp.jsx";
+import {Typography} from "@material-tailwind/react";
+import {useAuthStore} from "../../stores/authStore.js";
 
 const ProfilePage = () => {
 
-    const user = {
-        id: 1,
-        username: 'BetterPlace',
-        email: 'mail@mail.test',
-        level: 1,
-        experience: 0,
-        createdAt: '2024-09-16T11:21:15.997+00:00'
-    }
+    const {userInfo} = useAuthStore();
+    const user = userInfo.user;
 
     const posts = [
         {
@@ -52,8 +48,15 @@ const ProfilePage = () => {
                     <img src="https://placehold.co/150x150" alt={user.username + " profile picture"} />
                 </div>
                 <div className="profile_header_info">
-                    <h1 className="after:text-primary-400" data-level={user.level}>{user.username}</h1>
-                    <h3>Joined: {format(user.createdAt, 'dd.MM.yyyy')}</h3>
+                    <Typography as="h2" className="font-dev text-primary-400 text-center my-auto text-xl">
+                        Lv. {user.level}
+                    </Typography>
+                    <Typography as="h1" className="relative font-dev text-7xl text-center">
+                        {user.username}
+                    </Typography>
+                    <Typography as="h3" className="font-dev text-xl">
+                        Joined: {format(user.createdAt, 'dd.MM.yyyy')}
+                    </Typography>
                 </div>
             </div>
 
@@ -62,22 +65,28 @@ const ProfilePage = () => {
 
                 <div className="profile_info_stats">
                     <div>
-                        <h3>Follow</h3>
-                        <p>
-                            <CountUp to={16} />
-                        </p>
+                        <Typography as="h3" className="text-center font-dev text-2xl">
+                            Follow
+                        </Typography>
+                        <Typography className="text-center font-dev text-xl">
+                            <CountUp to={user.followedCount} />
+                        </Typography>
                     </div>
                     <div>
-                        <h3>Achievements</h3>
-                        <p>
-                            <CountUp to={4} />
-                        </p>
+                        <Typography as="h3" className="text-center font-dev text-2xl">
+                            Achievements
+                        </Typography>
+                        <Typography className="text-center font-dev text-xl">
+                            <CountUp to={user.achievements.length} />
+                        </Typography>
                     </div>
                     <div>
-                        <h3>Locks</h3>
-                        <p>
-                            <CountUp to={1234} separator={' '} />
-                        </p>
+                        <Typography as="h3" className="text-center font-dev text-2xl">
+                            Locks
+                        </Typography>
+                        <Typography className="text-center font-dev text-xl">
+                            <CountUp to={1234} />
+                        </Typography>
                     </div>
                 </div>
             </div>
