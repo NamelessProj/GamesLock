f2a543acf783713aefe5da1cf8e50dede17d8a99
Kevin <pintokevin2002@hotmail.com>
2024-10-24 21:43:32 +0200

    Creation of the messageStore.js

---
+++ frontend/src/stores/messageStore.js
@@ -0,0 +1,29 @@
+import {create} from "zustand";
+import axios from "axios";
+
+export const useMessageStore = create((set) => ({
+    userMessage: null,
+    messageLoading: false,
+    error: null,
+    success: false,
+
+    getAllMessages: async () => {
+        set(() => ({messageLoading: true, error: null}));
+        try{
+            const response = await axios.get('http://localhost:3000/api/message');
+            set(() => ({userMessage: response.data, messageLoading: false, success: true}));
+        }catch(error){
+            set({error: error.message, messageLoading: false});
+        }
+    },
+
+    getUserMessages: async (id) => {
+        set(() => ({messageLoading: true, error: null}));
+        try{
+            const response = await axios.get(`http://localhost:3000/api/message/${id}`);
+            set(() => ({userMessage: response.data, messageLoading: false, success: true}));
+        }catch(error){
+            set({error: error.message, messageLoading: false});
+        }
+    }
+}));
