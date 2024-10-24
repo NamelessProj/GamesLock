bc00dbff2d3ec39f0dc09ff90aa1e4a79da15dda
Kevin <pintokevin2002@hotmail.com>
2024-10-24 15:21:01 +0200

    Adding a way to logout from the navigation

--- frontend/src/components/SpeedNav.jsx
+++ frontend/src/components/SpeedNav.jsx
@@ -14,10 +14,15 @@ import SvgSearch from "./SVG/SvgSearch.jsx";
 import SvgPlus from "./SVG/SvgPlus.jsx";
 import SvgUser from "./SVG/SvgUser.jsx";
 import SvgBell from "./SVG/SvgBell.jsx";
+import {useAuthStore} from "../stores/authStore.js";
+import {useUserStore} from "../stores/userStore.js";
+import {LiaUserSlashSolid} from "react-icons/lia";
 
 const SpeedNav = () => {
-
     const [open, setOpen] = useState(false);
+
+    const {userInfo, logout} = useAuthStore();
+    const {userLogout} = useUserStore();
 
     const navigate = useNavigate();
 
@@ -25,6 +30,16 @@ const SpeedNav = () => {
         variant: "small",
         color: "blue-gray",
         className: "absolute top-2/4 -left-2/4 -translate-y-2/4 -translate-x-3/4 font-normal text-primary-900"
+    }
+
+    const handleLogout = async () => {
+        try{
+            await userLogout();
+            logout();
+            navigate('/');
+        }catch(e){
+            console.log(e);
+        }
     }
 
     return (
@@ -48,14 +63,27 @@ const SpeedNav = () => {
                         <SvgPlus className="h-5 w-5" />
                         <Typography {...labelProps}>Add</Typography>
                     </SpeedDialAction>
-                    <SpeedDialAction className="relative bg-gray-800 border-0" onClick={() => {navigate('/profile')}}>
-                        <SvgUser className="h-5 w-5" />
-                        <Typography {...labelProps}>Profile</Typography>
-                    </SpeedDialAction>
                     <SpeedDialAction className="relative bg-gray-800 border-0" onClick={() => {navigate('/notifications')}}>
                         <SvgBell className="h-5 w-5" />
                         <Typography {...labelProps}>Notifications</Typography>
                     </SpeedDialAction>
+                    {userInfo ? (
+                        <>
+                            <SpeedDialAction className="relative bg-gray-800 border-0" onClick={() => {navigate('/profile')}}>
+                                <SvgUser className="h-5 w-5" />
+                                <Typography {...labelProps}>Profile</Typography>
+                            </SpeedDialAction>
+                            <SpeedDialAction className="relative bg-gray-800 border-0" onClick={handleLogout}>
+                                <LiaUserSlashSolid className="h5 w-5" />
+                                <Typography {...labelProps}>Logout</Typography>
+                            </SpeedDialAction>
+                        </>
+                    ):(
+                        <SpeedDialAction className="relative bg-gray-800 border-0" onClick={() => {navigate('/login')}}>
+                            <SvgUser className="h-5 w-5" />
+                            <Typography {...labelProps}>Login</Typography>
+                        </SpeedDialAction>
+                    )}
                 </SpeedDialContent>
             </SpeedDial>
         </div>
