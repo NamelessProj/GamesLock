8954648d1416c45d65bcc8c7aafd43f12a862d9c
Kevin <pintokevin2002@hotmail.com>
2024-10-24 21:46:47 +0200

    Removing comments

--- frontend/src/pages/ProfilePage/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage/ProfilePage.jsx
@@ -22,7 +22,6 @@ const ProfilePage = () => {
             try{
                 await getUserMessages(user._id);
                 setPosts(userMessage);
-                console.log(posts)
             }catch(e){
                 console.log(e);
             }
@@ -30,36 +29,6 @@ const ProfilePage = () => {
 
         (async () => await fetchMessages()) ();
     }, []);
-
-    /*const posts = [
-        {
-            id: 3,
-            user: {
-                id: 1,
-                username: 'user1'
-            },
-            body: 'Hello everyone, nice to meet you!',
-            createdAt: '2024-10-18T15:30:23.254+02:00'
-        },
-        {
-            id: 2,
-            user: {
-                id: 2,
-                username: 'UserName'
-            },
-            body: 'I\'m here to stay.',
-            createdAt: '2024-10-18T12:11:33.254+02:00'
-        },
-        {
-            id: 1,
-            user: {
-                id: 1,
-                username: 'user1'
-            },
-            body: 'This is the body of post 1.',
-            createdAt: '2024-09-16T11:21:54.254+00:00'
-        }
-    ];*/
 
     return (
         <main>
