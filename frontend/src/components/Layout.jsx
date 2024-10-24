e2b5dc46b2f27cd86ae9a6863d134771a715df2b
Kevin <pintokevin2002@hotmail.com>
2024-10-24 16:15:15 +0200

    Adding the footer to the layout

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import Header from "./Header.jsx";
 import {Outlet} from "react-router-dom";
 import SpeedNav from "./SpeedNav.jsx";
+import Footer from "./Footer.jsx";
 
 const Layout = () => {
     return (
@@ -8,6 +9,7 @@ const Layout = () => {
             <Header />
             <Outlet />
             <SpeedNav />
+            <Footer />
         </div>
     );
 };
