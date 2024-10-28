11422f8fd7351594e0701f57b994b126d5ebec2e
Kevin Da Silva Pinto <pintokevin2002@hotmail.com>
2024-10-28 07:33:58 +0100

    Update Layout.jax
    Making the title of the page able to change

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -2,8 +2,14 @@ import Header from "./Header.jsx";
 import {Outlet} from "react-router-dom";
 import SpeedNav from "./SpeedNav.jsx";
 import Footer from "./Footer.jsx";
+import {useEffect} from "react";
 
-const Layout = () => {
+const Layout = ({title="GamesLock"}) => {
+    
+    useEffect(() => {
+        document.title = title;
+    }, [title]);
+    
     return (
         <div className="App relative">
             <Header />
