2c9cb7067388d505d3c1fd489d8771151042c867
Kevin <pintokevin2002@hotmail.com>
2024-10-29 17:04:43 +0100

    Updating the Posts.jsx style for the noPostMessage

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -8,7 +8,7 @@ const Posts = ({posts=[], noPostMessage="No posts to show."}) => {
                 posts.length ? (
                     posts.map((post, key) => (<Post key={key} post={post} />))
                 ):(
-                    <Typography className="text-center mx-auto">
+                    <Typography className="text-center mx-auto text-base">
                         {noPostMessage}
                     </Typography>
                 )
