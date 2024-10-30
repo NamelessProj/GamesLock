fecdafec0a2c598f8f2b8728467cc6ab6f1b8724
Kevin <pintokevin2002@hotmail.com>
2024-10-30 15:38:40 +0100

    Adding followed store for followedMessage

--- frontend/src/stores/messageStore.js
+++ frontend/src/stores/messageStore.js
@@ -4,8 +4,11 @@ import axios from "axios";
 export const useMessageStore = create((set) => ({
     userMessage: [],
     allMessages: [],
+    followedMessages: [],
     messageLoading: false,
+    followedMmessageLoading: false,
     error: null,
+    followedError: null,
     success: false,
 
     getAllMessages: async () => {
@@ -19,12 +22,12 @@ export const useMessageStore = create((set) => ({
     },
 
     getMessagesFromFollowedUsers: async (id) => {
-        set(() => ({messageLoading: true, error: null}));
+        set(() => ({followedMmessageLoading: true, followedError: null}));
         try{
             const response = await axios.get(`http://localhost:3000/api/message/followed/${id}`);
-            set(() => ({userMessage: response.data.messages, messageLoading: false, success: true}));
+            set(() => ({followedMessages: response.data.messages, followedMmessageLoading: false, success: true}));
         }catch(error){
-            set({error: error.message, messageLoading: false});
+            set({followedError: error.message, followedMmessageLoading: false});
         }
     },
 
