1a5ab6535f15215d71e94ddf6e55f8e6546a3d32
Kevin <pintokevin2002@hotmail.com>
2024-10-30 16:33:52 +0100

    Removing not needed "key" from the map

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -6,7 +6,7 @@ const Posts = ({posts=[], noPostMessage="No posts to show."}) => {
         <div className="w-full flex flex-col gap-8 pt-8">
             {
                 posts.length ? (
-                    posts.map((post, key) => (<Post key={key} post={post} />))
+                    posts.map((post) => (<Post key={post._id} post={post} />))
                 ):(
                     <Typography variant="lead" className="text-center mx-auto">
                         {noPostMessage}
