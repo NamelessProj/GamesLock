6e0a4ee96afed4f16200ef3ec8387a79c4dd3546
Kevin <pintokevin2002@hotmail.com>
2024-11-02 17:26:47 +0100

    Adding the new Achievement.jsx

---
+++ frontend/src/components/Achievement.jsx
@@ -0,0 +1,17 @@
+import {Typography} from "@material-tailwind/react";
+
+const Achievement = ({achievement}) => {
+    return (
+        <div className="flex flex-1 flex-col text-center text-balance">
+            <img src="https://placehold.co/30x30" alt={"Achievement: " + achievement.title} className="object-cover h-[200px]"/>
+            <Typography as="h2" className="">
+                {achievement.title}
+            </Typography>
+            <Typography variant="small" className="text-primary-900 opacity-50 text-base">
+                {achievement.description}
+            </Typography>
+        </div>
+    );
+};
+
+export default Achievement;
