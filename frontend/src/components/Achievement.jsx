5c6156793746a268ddb6376dcc291fb302324df9
Kevin <pintokevin2002@hotmail.com>
2024-11-02 17:29:24 +0100

    Adding class to the title

--- frontend/src/components/Achievement.jsx
+++ frontend/src/components/Achievement.jsx
@@ -4,7 +4,7 @@ const Achievement = ({achievement}) => {
     return (
         <div className="flex flex-1 flex-col text-center text-balance">
             <img src="https://placehold.co/30x30" alt={"Achievement: " + achievement.title} className="object-cover h-[200px]"/>
-            <Typography as="h2" className="">
+            <Typography as="h2" className="font-dev text-3xl">
                 {achievement.title}
             </Typography>
             <Typography variant="small" className="text-primary-900 opacity-50 text-base">
