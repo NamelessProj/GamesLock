49138f9a93ac0d9e48adaabfb335d218a2de7a98
Kevin <pintokevin2002@hotmail.com>
2024-11-07 15:39:09 +0100

    Adding i18n to the project

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -2,13 +2,17 @@ import Header from "./Header.jsx";
 import {Outlet} from "react-router-dom";
 import SpeedNav from "./SpeedNav.jsx";
 import Footer from "./Footer.jsx";
+import {Suspense} from "react";
+import {ScaleLoader} from "react-spinners";
 
 const Layout = () => {
     
     return (
         <div className="App relative grid grid-rows-app">
             <Header />
-            <Outlet />
+            <Suspense fallback={<ScaleLoader color="#bc4b27"/>}>
+                <Outlet />
+            </Suspense>
             <SpeedNav />
             <Footer />
         </div>
