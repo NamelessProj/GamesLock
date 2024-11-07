1baa6bd1fd525316dbe693c1ee0a3b2176614399
Kevin <pintokevin2002@hotmail.com>
2024-11-07 17:24:11 +0100

    Adding translation for the posts

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -1,7 +1,8 @@
 import Post from "./Post.jsx";
 import {Typography} from "@material-tailwind/react";
+import {Trans} from "react-i18next";
 
-const Posts = ({posts=[], noPostMessage="No posts to show."}) => {
+const Posts = ({posts=[], noPostMessage=<Trans i18nKey="posts.noPosts">No posts to show.</Trans>}) => {
     return (
         <div className="w-full flex flex-col gap-8 pt-8">
             {
