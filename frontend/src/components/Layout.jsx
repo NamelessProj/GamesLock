01cdffbf31e4e0d6969466039d22399bb8aebb13
Kevin <pintokevin2002@hotmail.com>
2024-11-13 11:08:45 +0100

    Adding the AppLoader to the layout

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,15 +1,15 @@
+import {Suspense} from "react";
+import AppLoader from "./AppLoader.jsx";
 import Header from "./Header.jsx";
 import {Outlet} from "react-router-dom";
 import SpeedNav from "./SpeedNav.jsx";
 import Footer from "./Footer.jsx";
 import "nprogress/nprogress.css";
-import {Suspense} from "react";
 
 const Layout = () => {
-    
     return (
         <div className="App relative grid grid-rows-app">
-            <Suspense fallback={<h1>Loading...</h1>}>
+            <Suspense fallback={<AppLoader />}>
                 <Header/>
                 <Outlet/>
                 <SpeedNav/>
