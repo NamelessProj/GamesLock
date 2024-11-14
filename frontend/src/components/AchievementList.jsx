963b7e98a31e1a09d42f6c1392f8f4de917605a2
Kevin <pintokevin2002@hotmail.com>
2024-11-14 14:56:14 +0100

    Sending the user to each Achievement

--- frontend/src/components/AchievementList.jsx
+++ frontend/src/components/AchievementList.jsx
@@ -1,14 +1,16 @@
 import Achievement from "./Achievement.jsx";
 import {Typography} from "@material-tailwind/react";
 import {useTranslation} from "react-i18next";
+import {useAuthStore} from "../stores/authStore.js";
 
 const AchievementList = ({achievements}) => {
     const {t} = useTranslation();
+    const {userInfo} = useAuthStore();
     return (
         <div className="grid grid-cols-achievements gap-3 p-1 mx-auto max-w-[1200px] justify-center">
             {achievements.length ? (
                 achievements.map((achievement) => (
-                    <Achievement key={achievement.id} achievement={achievement} />
+                    <Achievement key={achievement.id} achievement={achievement} user={userInfo} />
                 ))
             ):(
                 <Typography variant="lead" className="text-primary-900">
