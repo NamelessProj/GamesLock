aed27494d9eeede0088ab4773c680c9666044999
Kevin <pintokevin2002@hotmail.com>
2024-11-14 14:18:25 +0100

    Updating Achievement.jsx to display the correct name of each achievement

--- frontend/src/components/Achievement.jsx
+++ frontend/src/components/Achievement.jsx
@@ -3,9 +3,9 @@ import {Typography} from "@material-tailwind/react";
 const Achievement = ({achievement}) => {
     return (
         <div className="flex flex-1 flex-col text-center text-balance">
-            <img src="https://placehold.co/30x30" alt={"Achievement: " + achievement.title} className="object-cover h-[200px]"/>
+            <img src="https://placehold.co/30x30" alt={"Achievement: " + achievement.name} className="object-cover h-[200px]"/>
             <Typography as="h2" className="font-dev text-3xl">
-                {achievement.title}
+                {achievement.name}
             </Typography>
             <Typography variant="small" className="text-primary-900 opacity-50 text-base">
                 {achievement.description}
