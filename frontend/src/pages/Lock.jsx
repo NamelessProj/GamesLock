d369485677580d7512da2a732ecbec78f581fb96
Kevin <pintokevin2002@hotmail.com>
2024-11-15 14:29:15 +0100

    Updating the error is displayed

--- frontend/src/pages/Lock.jsx
+++ frontend/src/pages/Lock.jsx
@@ -6,11 +6,13 @@ import Post from "../components/Post.jsx";
 import {Alert} from "@material-tailwind/react";
 import CommentList from "../components/CommentList.jsx";
 import {useAuthStore} from "../stores/authStore.js";
+import {useTranslation} from "react-i18next";
 
 const Lock = () => {
     const {id} = useParams();
     const {getMessageById, message, messageLoading, error} = useMessageStore();
     const {userInfo} = useAuthStore();
+    const {t} = useTranslation();
 
     const [msg, setMsg] = useState(null);
     const [comments, setComments] = useState([]);
@@ -28,24 +30,32 @@ const Lock = () => {
 
     return (
         <main className="flex flex-col items-center">
-            {error ? (
-                <section>
+            {error && (
+                <section className="mb-6">
                     <Alert color="red">
                         {error}
                     </Alert>
                 </section>
-            ):(
-                <section>
-                    {messageLoading ? (
-                        <DefaultSpinner />
-                    ):(
-                        <div>
-                            {msg && <Post post={msg}/>}
-                            {comments && <CommentList comments={comments} user={userInfo}/>}
-                        </div>
-                    )}
-                </section>
             )}
+
+            <section>
+                {messageLoading ? (
+                    <DefaultSpinner />
+                ):(
+                    <div>
+                        {msg ? (
+                            <>
+                                <Post post={msg}/>
+                                {comments && <CommentList comments={comments} user={userInfo}/>}
+                            </>
+                        ):(
+                            <Alert color="red">
+                                {t("comment.noMsgFound")}
+                            </Alert>
+                        )}
+                    </div>
+                )}
+            </section>
         </main>
     );
 };
