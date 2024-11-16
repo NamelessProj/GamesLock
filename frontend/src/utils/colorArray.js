87c2b6428d696c142d26e853a7cb4437c9f48b17
Kevin <pintokevin2002@hotmail.com>
2024-11-16 16:39:47 +0100

    Adding an array of color in hsl format

---
+++ frontend/src/utils/colorArray.js
@@ -0,0 +1,14 @@
+export const colorArray = [
+    {
+        h: 0,
+        s: 100,
+        l: 50,
+        isDark: false
+    },
+    {
+        h: 120,
+        s: 100,
+        l: 50,
+        isDark: false
+    },
+];
