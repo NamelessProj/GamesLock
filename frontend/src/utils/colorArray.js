815f496eecea1a0c508e0af57f9b3097cac79285
Kevin <pintokevin2002@hotmail.com>
2024-11-16 18:21:58 +0100

    Adding colors

--- frontend/src/utils/colorArray.js
+++ frontend/src/utils/colorArray.js
@@ -11,4 +11,28 @@ export const colorArray = [
         l: 50,
         isDark: false
     },
+    {
+        h: 240,
+        s: 100,
+        l: 50,
+        isDark: false
+    },
+    {
+        h: 60,
+        s: 100,
+        l: 50,
+        isDark: false
+    },
+    {
+        h: 180,
+        s: 100,
+        l: 50,
+        isDark: false
+    },
+    {
+        h: 300,
+        s: 100,
+        l: 50,
+        isDark: false
+    },
 ];
