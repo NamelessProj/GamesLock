8f7472d40a305dae2f44f7e54b8c546ec59cc5fb
Kevin <pintokevin2002@hotmail.com>
2024-11-17 11:27:15 +0100

    Using the new MagnetComponent.jsx

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -1,17 +1,21 @@
 import {Link} from "react-router-dom";
+import MagnetComponent from "../components/MagnetComponent.jsx";
 import GradientText from "../components/GradientText.jsx";
 import {Typography} from "@material-tailwind/react";
 import {useTranslation} from "react-i18next";
 
 const ErrorPage = () => {
     const {t} = useTranslation();
+
     return (
-        <main className="page-not-found flex flex-col justify-center items-center">
-            <GradientText colors={['#bc4b27', '#fddc75', '#bc4b27', '#fddc75']}>
-                <Typography as="h1" variant="h1" className="font-dev text-8xl">
-                    404
-                </Typography>
-            </GradientText>
+        <main className="page-not-found flex flex-col justify-center items-center gap-6">
+            <MagnetComponent>
+                <GradientText colors={['#bc4b27', '#fddc75', '#bc4b27', '#fddc75']}>
+                    <Typography as="h1" variant="h1" className="font-dev text-8xl">
+                        404
+                    </Typography>
+                </GradientText>
+            </MagnetComponent>
             <div className="flex flex-col gap-3">
                 <Typography as="h2" variant="h2" className="text-center text-3xl font-bold">
                     {t("error404.title")}
