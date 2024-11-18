a33d5ea271562675a1b7c865212037b30b1eaa9a
Pinto <pintokevin2002@hotmail.com>
2024-11-18 09:31:44 +0100

    Removing NProgress import

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -4,7 +4,6 @@ import Header from "./Header.jsx";
 import {Outlet} from "react-router-dom";
 import SpeedNav from "./SpeedNav.jsx";
 import Footer from "./Footer.jsx";
-import "nprogress/nprogress.css";
 
 const Layout = () => {
     return (
