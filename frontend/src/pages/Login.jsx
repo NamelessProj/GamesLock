346a4cbe1e1ae64e36af0e5a5dfd8328b7f4684c
Kevin <pintokevin2002@hotmail.com>
2024-11-19 11:20:39 +0100

    Before login, checking if there's a user

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     useEffect(() => {
-        if(userSuccess){
+        if(userSuccess && user){
             setCredentials({user});
             navigate('/');
         }
