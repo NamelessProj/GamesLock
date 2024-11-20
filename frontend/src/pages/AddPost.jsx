b7815925f3855054f3ab803e77e034b23ff347f0
Kevin <pintokevin2002@hotmail.com>
2024-11-20 01:00:15 +0100

    Adding the page to add posts

---
+++ frontend/src/pages/AddPost.jsx
@@ -0,0 +1,44 @@
+import {Button, Input, Typography} from "@material-tailwind/react";
+import {useTranslation} from "react-i18next";
+import {useState} from "react";
+
+const AddPost = () => {
+    const [text, setText] = useState('');
+
+    const {t} = useTranslation();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(text);
+    }
+
+    return (
+        <main className="flex justify-center items-center">
+            <section className="my-6">
+                <Typography as="h1">
+                    Add Post
+                </Typography>
+                <form>
+                    <div className="flex flex-col gap-6">
+                        <Input
+                            name="text"
+                            placeholder="Post text"
+                            label="Post text"
+                            variant="static"
+                            value={text}
+                            onChange={(e) => setText(e.target.value)}
+                        />
+                        <Button
+                            color="deep-orange"
+                            onClick={handleSubmit}
+                        >
+                            Submit
+                        </Button>
+                    </div>
+                </form>
+            </section>
+        </main>
+    );
+};
+
+export default AddPost;
