63179056995482d912c45b0c6efc73ca07b436a0
Kevin <pintokevin2002@hotmail.com>
2024-11-21 00:06:34 +0100

    Adding InputPassword.jsx

---
+++ frontend/src/components/InputPassword.jsx
@@ -0,0 +1,42 @@
+import {useState} from "react";
+import {Input} from "@material-tailwind/react";
+import {FaEye, FaEyeSlash} from "react-icons/fa";
+
+const InputPassword = ({value, handler, size="lg", name="password", variant="standard", color="deep-orange", label="Password", iconColor="#bc4b27", iconSize=19, containerClass="", inputClass="", buttonClass=""}) => {
+    const [type, setType] = useState('password');
+
+    const toggleType = () => {setType(type === "text" ? "password" : "text")}
+
+    const iconProps = {
+        color: iconColor,
+        size: type === "text" ? iconSize + 2 : iconSize,
+    };
+
+    const Button = ({onClick}) => {
+        return (
+            <div onClick={onClick} className={`absolute right-2 top-0 bottom-0 cursor-pointer flex justify-center items-center ${buttonClass}`}>
+                {type === "text" ? <FaEyeSlash {...iconProps} /> : <FaEye {...iconProps} />}
+            </div>
+        );
+    };
+
+    return (
+        <div className={`relative ${containerClass}`}>
+            <Input
+                type={type}
+                name={name}
+                label={label}
+                variant={variant}
+                color={color}
+                value={value}
+                size={size}
+                onChange={(e) => handler(e.target.value)}
+                icon={<Button onClick={toggleType}/>}
+                containerProps={{color: "red"}}
+                className={`text-primary-900 ${inputClass}`}
+            />
+        </div>
+    );
+};
+
+export default InputPassword;
