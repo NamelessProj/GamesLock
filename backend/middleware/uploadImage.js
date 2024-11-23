03e111778ae13db4903d7abe69d150a8f7b29926
Kevin <pintokevin2002@hotmail.com>
2024-11-23 17:48:21 +0100

    Adding a middleware to upload images

---
+++ backend/middleware/uploadImage.js
@@ -0,0 +1,31 @@
+const multer = require('multer');
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, './uploads');
+        },
+        filename: (req, file, cb) => {
+            cb(null, `${file.fieldname}-${Date.now()}${file.originalname}`);
+        }
+    }),
+    fileFilter: (req, file, cb) => {
+        if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+            cb(null, true);
+        }else{
+            cb(new Error('Only .jpeg or .png files are allowed'));
+        }
+        if (!file.originalname.match(/\.(jpeg|jpg|png)$/)){
+            return cb(
+                new Error(
+                    'only upload files with jpg, jpeg, png, pdf, doc, docx, xslx, xls format.'
+                )
+            );
+        }
+        cb(undefined, true);
+    }
+});
+
+module.exports = {
+    upload,
+};
