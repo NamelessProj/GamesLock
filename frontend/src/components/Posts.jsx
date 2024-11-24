fc20d3cebe2c575ecdc6bdb4353664dbf134ee4e
Kevin <pintokevin2002@hotmail.com>
2024-11-24 17:23:21 +0100

    Replacing the Dialog with the new Dialog component

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -1,8 +1,8 @@
 import Post from "./Post.jsx";
-import {Button, Dialog, DialogFooter, DialogHeader, Typography} from "@material-tailwind/react";
+import {Typography} from "@material-tailwind/react";
 import {useTranslation} from "react-i18next";
 import {useState} from "react";
-import {Link} from "react-router-dom";
+import DialogLikePost from "./DialogLikePost.jsx";
 
 const Posts = ({posts=[], noPostMessage=""}) => {
     const {t} = useTranslation();
@@ -11,20 +11,7 @@ const Posts = ({posts=[], noPostMessage=""}) => {
 
     return (
         <div className="w-full flex flex-col gap-10 pt-8">
-            <Dialog open={openDialog} handler={handleOpenDialog} size="xs">
-                <DialogHeader>
-                    <Typography variant="h4" className="w-full text-center text-balance">
-                        {t("posts.loginToLike")}
-                    </Typography>
-                </DialogHeader>
-                <DialogFooter className="flex justify-center items-center">
-                    <Button color="deep-orange" variant="gradient">
-                        <Link to="/login">
-                            {t("register.login.link")}
-                        </Link>
-                    </Button>
-                </DialogFooter>
-            </Dialog>
+            <DialogLikePost open={openDialog} handler={handleOpenDialog} />
             {
                 posts.length ? (
                     posts.map((post, key) => (<Post key={key} post={post} handleDialog={handleOpenDialog} />))
