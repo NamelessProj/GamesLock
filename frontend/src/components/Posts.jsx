b7d6e3caabb642198038a2947896f34f188a661f
Kevin <pintokevin2002@hotmail.com>
2024-11-24 16:56:36 +0100

    Adding a dialog to tell user to login when trying to like a post

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -1,13 +1,33 @@
 import Post from "./Post.jsx";
-import {Typography} from "@material-tailwind/react";
-import {Trans} from "react-i18next";
+import {Button, Dialog, DialogFooter, DialogHeader, Typography} from "@material-tailwind/react";
+import {Trans, useTranslation} from "react-i18next";
+import {useState} from "react";
+import {Link} from "react-router-dom";
 
 const Posts = ({posts=[], noPostMessage=<Trans i18nKey="posts.noPosts">No posts to show.</Trans>}) => {
+    const {t} = useTranslation();
+    const [openDialog, setOpenDialog] = useState(false);
+    const handleOpenDialog = () => setOpenDialog(!openDialog);
+
     return (
         <div className="w-full flex flex-col gap-10 pt-8">
+            <Dialog open={openDialog} handler={handleOpenDialog} size="xs">
+                <DialogHeader>
+                    <Typography variant="h4" className="w-full text-center text-balance">
+                        {t("posts.loginToLike")}
+                    </Typography>
+                </DialogHeader>
+                <DialogFooter className="flex justify-center items-center">
+                    <Button color="deep-orange" variant="gradient">
+                        <Link to="/login">
+                            {t("register.login.link")}
+                        </Link>
+                    </Button>
+                </DialogFooter>
+            </Dialog>
             {
                 posts.length ? (
-                    posts.map((post, key) => (<Post key={key} post={post} />))
+                    posts.map((post, key) => (<Post key={key} post={post} handleDialog={handleOpenDialog} />))
                 ):(
                     <Typography variant="lead" className="text-center mx-auto text-primary-900">
                         {noPostMessage}
