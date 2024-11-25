3a1cf4b57ad2452dc2d3104ca6b548c4f01a67a3
Pinto <pintokevin2002@hotmail.com>
2024-11-25 14:31:49 +0100

    Updating the way the hsl is returned

--- frontend/src/utils/getRandomColorSeeded.js
+++ frontend/src/utils/getRandomColorSeeded.js
@@ -1,7 +1,10 @@
 import Rand from "rand-seed";
-import {colorArray} from "./colorArray.js";
 
 export const getRandomColorSeeded = (seed) => {
     const rand = new Rand(seed);
-    return colorArray[Math.floor((colorArray.length + 1) * rand.next())];
+    return {
+        h: Math.floor(351 * rand.next()),
+        s: 100,
+        l: 50
+    };
 }
