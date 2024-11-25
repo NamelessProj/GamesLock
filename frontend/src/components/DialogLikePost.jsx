6136895968dba852e145267c2a683a6b4d48a4c5
Pinto <pintokevin2002@hotmail.com>
2024-11-25 10:18:52 +0100

    Updating the Dialog to animate his entrance and adding a cancel button

--- frontend/src/components/DialogLikePost.jsx
+++ frontend/src/components/DialogLikePost.jsx
@@ -6,14 +6,25 @@ const DialogLikePost = ({open, handler}) => {
     const {t} = useTranslation();
 
     return (
-        <Dialog open={open} handler={handler} size="xs">
+        <Dialog
+            open={open}
+            handler={handler}
+            size="xs"
+            animate={{
+                mount: {scale: 1},
+                unmount: {scale: 0.9}
+            }}
+        >
             <DialogHeader>
                 <Typography variant="h4" className="w-full text-center text-balance">
                     {t("posts.loginToLike")}
                 </Typography>
             </DialogHeader>
-            <DialogFooter className="flex justify-center items-center">
-                <Button color="deep-orange" variant="gradient">
+            <DialogFooter className="flex justify-center items-center gap-6">
+                <Button color="gray" variant="text" onClick={handler}>
+                    {t("posts.cancelDialog")}
+                </Button>
+                <Button color="deep-orange" variant="filled">
                     <Link to="/login">
                         {t("register.login.link")}
                     </Link>
