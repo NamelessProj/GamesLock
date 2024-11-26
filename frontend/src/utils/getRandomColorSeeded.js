b3f96578276dc0dc5ca4c85d30be889372e47f3f
Pinto <pintokevin2002@hotmail.com>
2024-11-26 11:15:25 +0100

    Updating the way the seed is used by making sure it's a string and that it's all uppercase.

--- frontend/src/utils/getRandomColorSeeded.js
+++ frontend/src/utils/getRandomColorSeeded.js
@@ -1,7 +1,7 @@
 import Rand from "rand-seed";
 
 export const getRandomColorSeeded = (seed) => {
-    const rand = new Rand(seed);
+    const rand = new Rand(seed.toString().toUpperCase());
     return {
         h: Math.floor(351 * rand.next()),
         s: 100,
