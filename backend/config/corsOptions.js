5fa788989f834a8a0236ba3e30d85ff6e5b09269
Pinto <pintokevin2002@hotmail.com>
2024-11-26 11:35:55 +0100

    Updating the CORS, so only the frontend can access the backend

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -2,7 +2,7 @@ const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if(allowedOrigins.indexOf(origin) !== -1 || !origin){
+        if(allowedOrigins.includes(origin)){
             callback(null, true);
         }else{
             callback(new Error("Origins not allowed by CORS."));
