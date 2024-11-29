e4ff848331eac005a0fc45fafe2cfc103a1332c6
Kevin <pintokevin2002@hotmail.com>
2024-11-29 15:26:01 +0100

    Adding a way to get Postman to check routes

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -2,7 +2,7 @@ const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if(allowedOrigins.includes(origin)){
+        if(allowedOrigins.includes(origin) || !origin){
             callback(null, true);
         }else{
             callback(new Error("Origins not allowed by CORS."));
