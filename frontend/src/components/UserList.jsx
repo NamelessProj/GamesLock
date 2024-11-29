1947870c9e20ab24bcfbce7641a7e9c6ab7cbd3a
Kevin <pintokevin2002@hotmail.com>
2024-11-29 16:58:37 +0100

    Adding the UserList.jsx component

---
+++ frontend/src/components/UserList.jsx
@@ -0,0 +1,23 @@
+import UserTrailer from "./UserTrailer.jsx";
+import {getPostLocale} from "../utils/getPostLocale.js";
+import {Typography} from "@material-tailwind/react";
+import {useTranslation} from "react-i18next";
+
+const UserList = ({users, noUserMessage=""}) => {
+    const {t} = useTranslation();
+    const locale = getPostLocale();
+
+    return (
+        <div className="w-post mx-auto flex flex-col pt-8">
+            {users.length ? (
+                users.map((user, key) => (<UserTrailer key={key} user={user} locale={locale}/>))
+            ):(
+                <Typography variant="lead" className="text-center mx-auto text-primary-900">
+                    {noUserMessage === "" ? t("profile.list.noUsers") : noUserMessage}
+                </Typography>
+            )}
+        </div>
+    );
+};
+
+export default UserList;
