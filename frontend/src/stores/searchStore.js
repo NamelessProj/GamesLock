70b987d4080a106bd4d3622d2b10b9cd8cf9c526
Kevin <pintokevin2002@hotmail.com>
2024-11-29 16:01:08 +0100

    Creating a store to make search request

---
+++ frontend/src/stores/searchStore.js
@@ -0,0 +1,29 @@
+import {create} from "zustand";
+import axios from "axios";
+
+export const useSearchStore = create((set) => ({
+    searchUsers: [],
+    searchGames: [],
+    searchLoading: false,
+    searchError: null,
+
+    searchUser: async (string) => {
+        set(() => ({searchLoading: true, searchError: null}));
+        try{
+            const response = await axios.get(`${import.meta.env.VITE_API_URL}search/users/${string}`);
+            set(() => ({searchUsers: response.data.users, searchLoading: false}));
+        }catch(error){
+            set({searchError: error.message, searchLoading: false});
+        }
+    },
+
+    searchGame: async (game) => {
+        set(() => ({searchLoading: true, searchError: null}));
+        try{
+            const response = await axios.get(`${import.meta.env.VITE_API_URL}search/game/${game}`);
+            set(() => ({searchGames: response.data.messages, searchLoading: false}));
+        }catch(error){
+            set({searchError: error.message, searchLoading: false});
+        }
+    },
+}));
