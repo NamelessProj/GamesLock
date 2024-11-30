1485617e4d93b530adbdba11f7341c3b65eebfc2
Kevin <pintokevin2002@hotmail.com>
2024-11-30 16:00:38 +0100

    Using the DataContext

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,17 +5,20 @@ import './style.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import {ThemeProvider} from "@material-tailwind/react";
 import "./utils/NProgressConfig.js";
+import {DataProvider} from "./context/DataContext.jsx";
 
 import './i18n.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider>
-        <Router>
-            <Routes>
-                <Route path="/*" element={<App />} />
-            </Routes>
-        </Router>
-    </ThemeProvider>
+    <DataProvider>
+        <ThemeProvider>
+            <Router>
+                <Routes>
+                    <Route path="/*" element={<App />} />
+                </Routes>
+            </Router>
+        </ThemeProvider>
+    </DataProvider>
   </StrictMode>,
 );
