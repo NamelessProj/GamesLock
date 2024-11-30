10ac195a77ec0a5e033115e9298724129b4ed771
Kevin <pintokevin2002@hotmail.com>
2024-11-30 23:02:51 +0100

    Updating the way the user can access his profile

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -1,43 +1,10 @@
-import ProfileHeader from "../components/ProfileHeader.jsx";
 import {useAuthStore} from "../stores/authStore.js";
-import {useMessageStore} from "../stores/messageStore.js";
 import {Navigate} from "react-router-dom";
-import {useEffect} from "react";
-import ProfileMessages from "../components/ProfileMessages.jsx";
-import NProgress from "nprogress";
 
 const UserProfile = () => {
     const {userInfo} = useAuthStore();
-    const {userMessage, getUserMessages, error, messageLoading} = useMessageStore();
-
-    const user = userInfo ? userInfo.user : null;
-
-    useEffect(() => {
-        const fetchMessages = async () => {
-            NProgress.start();
-            try{
-                await getUserMessages(user?._id);
-            }catch(e){
-                console.log(e);
-            }
-            NProgress.done();
-        }
-
-        (async () => await fetchMessages()) ();
-    }, []);
-
     return (
-        <main>
-            {user ? (
-                <>
-                    <ProfileHeader user={user} userMessage={userMessage}/>
-                    <div className="separator h-0.5 rounded-full bg-primary-900 opacity-50 mx-auto my-5 w-sp-1"></div>
-                    <ProfileMessages messageLoading={messageLoading} userMessage={userMessage} error={error} />
-                </>
-            ):(
-                <Navigate to="/login"/>
-            )}
-        </main>
+        <Navigate to={userInfo ? `/profile/${userInfo.user._id}` : '/login'} />
     );
 };
 
