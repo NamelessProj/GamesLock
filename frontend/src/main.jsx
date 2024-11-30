4a4bf50c640ff4204d7a9ddd659984d837d8eda2
Kevin <pintokevin2002@hotmail.com>
2024-11-30 16:45:51 +0100

    Updating the way elements are imported in the main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,24 +1,23 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
 import './style.css';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import {ThemeProvider} from "@material-tailwind/react";
-import "./utils/NProgressConfig.js";
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 import {DataProvider} from "./context/DataContext.jsx";
-
+import {ThemeProvider} from "@material-tailwind/react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import App from './App.jsx';
+import "./utils/NProgressConfig.js";
 import './i18n.js';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <DataProvider>
-        <ThemeProvider>
-            <Router>
-                <Routes>
-                    <Route path="/*" element={<App />} />
-                </Routes>
-            </Router>
-        </ThemeProvider>
-    </DataProvider>
-  </StrictMode>,
+    <StrictMode>
+        <DataProvider>
+            <ThemeProvider>
+                <Router>
+                    <Routes>
+                        <Route path="/*" element={<App />} />
+                    </Routes>
+                </Router>
+            </ThemeProvider>
+        </DataProvider>
+    </StrictMode>,
 );
