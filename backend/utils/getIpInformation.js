6cf9e70dd551e9681fccd35fdd0731ce7a314678
Kevin <pintokevin2002@hotmail.com>
2024-12-02 10:16:11 +0100

    Removing a console.log

--- backend/utils/getIpInformation.js
+++ backend/utils/getIpInformation.js
@@ -4,7 +4,6 @@ const fetch = require("node-fetch");
 const getIpInformation = async (req) => {
     const currentIp = requestIp.getClientIp(req);
     const url = `https://ipapi.co/${currentIp}/json/`;
-    console.log(url)
     const ipFetch = await fetch(url);
     const ipData = await ipFetch.json();
     return {
