9da80b3aff4d2ea47dc93d11f1f880b648f3a372
Kevin <pintokevin2002@hotmail.com>
2024-12-02 20:21:23 +0100

    Updating CopyInClipboard.jsx to add ClassName props

--- frontend/src/components/CopyInClipboard.jsx
+++ frontend/src/components/CopyInClipboard.jsx
@@ -1,13 +1,13 @@
 import {useState} from "react";
 import {Button, Typography} from "@material-tailwind/react";
 
-const CopyInClipboard = ({text=""}) => {
+const CopyInClipboard = ({value="", className="", textClassName="", buttonClassName=""}) => {
     const [copySuccess, setCopySuccess] = useState('');
 
     const handleCopy = async (e) => {
         e.preventDefault();
         try{
-            await navigator.clipboard.writeText(text);
+            await navigator.clipboard.writeText(value);
             setCopySuccess('Copied!');
         }catch(e){
             console.error(e);
@@ -18,15 +18,17 @@ const CopyInClipboard = ({text=""}) => {
     }
 
     return (
-        <div className="relative rounded-xl max-w-sm mx-auto bg-gray-800 px-4 py-2">
-            <Typography variant="lead">
-                {text}
-            </Typography>
+        <div className={`relative rounded-xl max-w-sm bg-gray-800 px-4 py-2 ${className}`}>
+            <div className="overflow-hidden">
+                <Typography variant="lead" className={`${textClassName}`}>
+                    {value}
+                </Typography>
+            </div>
             <Button
                 onClick={handleCopy}
                 color="deep-orange"
                 variant="gradient"
-                className="!absolute right-1 top-1/2 transform -translate-y-1/2"
+                className={`!absolute right-1 top-1/2 transform -translate-y-1/2 ${buttonClassName}`}
             >
                 {copySuccess ? copySuccess : 'Copy'}
             </Button>
