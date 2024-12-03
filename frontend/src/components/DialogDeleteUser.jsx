7e109f80b85544761492bdb579e80b519c2ef9bb
Kevin <pintokevin2002@hotmail.com>
2024-12-03 11:24:56 +0100

    Adding delete dialog translation

--- frontend/src/components/DialogDeleteUser.jsx
+++ frontend/src/components/DialogDeleteUser.jsx
@@ -1,6 +1,9 @@
-import {Button, Dialog, DialogFooter, DialogHeader, Typography} from "@material-tailwind/react";
+import {Button, Dialog, DialogBody, DialogFooter, DialogHeader, Typography} from "@material-tailwind/react";
+import {useTranslation} from "react-i18next";
 
 const DialogDeleteUser = ({open, handle, handleConfirm}) => {
+    const {t} = useTranslation();
+
     const handleClick = (e) => {
         e.preventDefault();
         handleConfirm(true);
@@ -19,15 +22,20 @@ const DialogDeleteUser = ({open, handle, handleConfirm}) => {
         >
             <DialogHeader>
                 <Typography variant="h4" className="w-full text-center text-balance">
-                    Are you sure you want to delete your account?
+                    {t("profile.deleteDialog.title")}
                 </Typography>
             </DialogHeader>
+            <DialogBody>
+                <Typography className="text-center text-balance">
+                    {t("profile.deleteDialog.text")}
+                </Typography>
+            </DialogBody>
             <DialogFooter className="flex justify-center items-center gap-6">
                 <Button color="green" variant="text" onClick={handle}>
-                    Cancel
+                    {t("profile.deleteDialog.cancel")}
                 </Button>
                 <Button color="red" variant="gradient" onClick={handleClick}>
-                    Delete This Account
+                    {t("profile.deleteDialog.delete")}
                 </Button>
             </DialogFooter>
         </Dialog>
