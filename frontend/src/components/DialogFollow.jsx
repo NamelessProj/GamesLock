0a8a5616b8ac7412a817dbbeb8db3355e21f11f5
Kevin <pintokevin2002@hotmail.com>
2024-12-03 11:31:00 +0100

    Adding log in to follow dialog translation

--- frontend/src/components/DialogFollow.jsx
+++ frontend/src/components/DialogFollow.jsx
@@ -1,9 +1,11 @@
-import {Button, Dialog, DialogFooter, DialogHeader, Typography} from "@material-tailwind/react";
+import {Button, Dialog, DialogBody, DialogFooter, DialogHeader, Typography} from "@material-tailwind/react";
 import {useNavigate} from "react-router-dom";
 import {useContext} from "react";
 import DataContext from "../context/DataContext.jsx";
+import {useTranslation} from "react-i18next";
 
 const DialogFollow = ({open, handle, userId}) => {
+    const {t} = useTranslation();
     const navigate = useNavigate();
     const {setBackUrl} = useContext(DataContext);
 
@@ -25,15 +27,20 @@ const DialogFollow = ({open, handle, userId}) => {
         >
             <DialogHeader>
                 <Typography variant="h4" className="w-full text-center text-balance">
-                    Please login to follow the user.
+                    {t("profile.loginFollowDialog.title")}
                 </Typography>
             </DialogHeader>
+            <DialogBody>
+                <Typography className="text-center text-balance">
+                    {t("profile.loginFollowDialog.text")}
+                </Typography>
+            </DialogBody>
             <DialogFooter className="flex justify-center items-center gap-6">
                 <Button color="red" variant="text" onClick={handle}>
-                    Cancel
+                    {t("profile.loginFollowDialog.cancel")}
                 </Button>
                 <Button color="deep-orange" variant="gradient" onClick={handleNavigate}>
-                    Login
+                    {t("profile.loginFollowDialog.follow")}
                 </Button>
             </DialogFooter>
         </Dialog>
