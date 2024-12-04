fb50ebca6014a25db18c3b081b69f7f70e2a9549
Kevin <pintokevin2002@hotmail.com>
2024-12-04 10:06:41 +0100

    Adding an Easter Egg when doing the Konami code

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -1,14 +1,51 @@
 import {Link} from "react-router-dom";
 import MagnetComponent from "../components/MagnetComponent.jsx";
 import GradientText from "../components/GradientText.jsx";
-import {Typography} from "@material-tailwind/react";
+import {Dialog, DialogBody, DialogHeader, IconButton, Typography} from "@material-tailwind/react";
 import {useTranslation} from "react-i18next";
+import {useKonamiCode} from "@bitmap/use-konami-code";
+import {useState} from "react";
+import {RxCross2} from "react-icons/rx";
 
 const ErrorPage = () => {
     const {t} = useTranslation();
+    const [openDialog, setOpenDialog] = useState(false);
+    const handleOpenDialog = () => setOpenDialog(!openDialog);
+
+    useKonamiCode(() => {
+        setOpenDialog(true);
+    });
+
+     // Showing a YouTube video on how to do the Konami code when the user invert the "a" and "b" keys
+    const invertedKonamiCode = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "a", "b", "Enter"];
+    useKonamiCode(() => {
+        window.open("https://www.youtube.com/watch?v=wece_wR_u9s");
+    }, invertedKonamiCode);
 
     return (
         <main className="page-not-found flex flex-col justify-center items-center gap-6">
+            <Dialog open={openDialog} handler={handleOpenDialog}>
+                <DialogHeader className="relative flex justify-center items-center">
+                    <Typography variant="h2" className="text-center text-balance">
+                        You Cheated!
+                    </Typography>
+                    <IconButton
+                        color="red"
+                        variant="text"
+                        className="!absolute top-2 right-2"
+                        onClick={handleOpenDialog}
+                    >
+                        <RxCross2 size={24}/>
+                        <Typography className="sr-only">
+                            {t("posts.share.close")}
+                        </Typography>
+                    </IconButton>
+                </DialogHeader>
+                <DialogBody className="flex justify-center items-center">
+                    <img src="/giphy.webp" alt="Cheater Detected!" loading="lazy" className="rounded-xl"/>
+                </DialogBody>
+            </Dialog>
+
             <MagnetComponent>
                 <GradientText colors={['#bc4b27', '#fddc75', '#bc4b27', '#fddc75']}>
                     <Typography as="h1" variant="h1" className="font-dev text-8xl">
