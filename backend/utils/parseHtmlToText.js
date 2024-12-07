70e2a4a3c74bff4a3219c1cfdedb033d41064412
Kevin <pintokevin2002@hotmail.com>
2024-12-07 19:11:05 +0100

    Renaming the function

--- backend/utils/parseHtmlToText.js
+++ backend/utils/parseHtmlToText.js
@@ -1,4 +1,4 @@
-export const parseHtml = (html) => {
+export const parseHtmlToText = (html) => {
     const regex = /<(\/\w*|\w+(\s?\w+(=(["'])[^<>]*\4)?)*\s?\/?)>/gm; // J'ai passé 25 minutes pour créer cette regex
     return html.replace(regex, ' ');
 }
