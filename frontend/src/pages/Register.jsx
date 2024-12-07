126ada64ab0e65d88842d2b661528a1b0a470d0f
Kevin Da Silva Pinto <pintokevin2002@hotmail.com>
2024-12-07 12:46:29 +0100

    Update Register.jsx
    Reducing useless span

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -92,7 +92,7 @@ const Register = () => {
                 ) : (
                     <Card className="w-96 bg-gray-800" role="form">
                         <CardHeader className="grid h-36 place-items-start bg-[url('/bg-register.png')] bg-no-repeat bg-cover">
-                            <span></span>
+                            <span/>
                         </CardHeader>
 
                         <CardBody className="flex flex-col gap-8">
