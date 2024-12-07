ab1d772d8495361b9d58c19ab7bcd063c11be4a5
Kevin <pintokevin2002@hotmail.com>
2024-12-07 19:05:42 +0100

    Adding a function to remove html tags and return plain text

---
+++ backend/utils/parseHtml.js
@@ -0,0 +1,4 @@
+export const parseHtml = (html) => {
+    const regex = /<(\/\w*|\w+(\s?\w+(=(["'])[^<>]*\4)?)*\s?\/?)>/gm; // J'ai passé 25 minutes pour créer cette regex
+    return html.replace(regex, ' ');
+}
