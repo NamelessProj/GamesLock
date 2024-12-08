22837f1d16680cd9cbc7e4c2c073c51ac09754e3
Kevin Da Silva Pinto <pintokevin2002@hotmail.com>
2024-12-08 19:10:57 +0100

    Update parseHtmlToText.js
    Updating the regex to check more than one case

--- backend/utils/parseHtmlToText.js
+++ backend/utils/parseHtmlToText.js
@@ -1,7 +1,7 @@
 const parseHtmlToText = (html) => {
     const regex = /<(\/\w*|\w+(\s?\w+(=(["'])[^<>]*\4)?)*\s?\/?)>/gm; // J'ai passé 25 minutes pour créer cette regex
     const text = html.replace(regex, ' ');
-    return text.replace(/\s{2,}/, ' '); // Removing all double or more spaces
+    return text.replace(/\s{2,}/gm, ' '); // Removing all double or more spaces
 }
 
 module.exports = {
