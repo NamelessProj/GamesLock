a1de9acf879942a4ad0b3ae9b427bafd8134d94a
Kevin Da Silva Pinto <pintokevin2002@hotmail.com>
2024-12-08 19:05:47 +0100

    Update parseHtmlToText.js
    Remonving double or more spaces

--- backend/utils/parseHtmlToText.js
+++ backend/utils/parseHtmlToText.js
@@ -1,6 +1,7 @@
 const parseHtmlToText = (html) => {
     const regex = /<(\/\w*|\w+(\s?\w+(=(["'])[^<>]*\4)?)*\s?\/?)>/gm; // J'ai passé 25 minutes pour créer cette regex
-    return html.replace(regex, ' ');
+    const text = html.replace(regex, ' ');
+    return text.replace(/\s{2,}/, ' '); // Removing all double or more spaces
 }
 
 module.exports = {
