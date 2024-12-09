6734988e77ed166882f9607b0ff939ac8ce1cb6f
Kevin <pintokevin2002@hotmail.com>
2024-12-09 12:25:43 +0100

    Adding a cron-job to delete every LOGs that are older than 3 month

--- backend/controllers/logController.js
+++ backend/controllers/logController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const Log = require('../models/logModel');
+const cron = require('node-cron');
 
 // @desc Deleting a log with an id
 // @route DELETE /api/log/delete/:_id
@@ -22,6 +23,20 @@ const deleteAllLogs = asyncHandler(async (req, res) => {
     await Log.deleteMany({user: req.user._id});
     res.status(200).json({message: `All the logs have been deleted successfully.`});
 });
+
+
+
+/*-----------------------------------------*/
+/*                  CRON JOM               */
+/*-----------------------------------------*/
+
+
+cron.schedule('0 3 1 * *', async () => {
+    // Deleting all the logs that are older than 3 months every 1st day of the month at 3:00 AM
+    const currentDate = new Date();
+    await Log.deleteMany({createdAt: {$lt: currentDate.setMonth(currentDate.getMonth() - 3)}});
+});
+
 
 module.exports = {
     deleteALog,
