a090cf24525cc91bdb45f45ec8b79ea8c383b7f3
Kevin <pintokevin2002@hotmail.com>
2024-12-10 19:56:04 +0100

    Updating the Notifications page

--- frontend/src/pages/Notifications.jsx
+++ frontend/src/pages/Notifications.jsx
@@ -1,15 +1,16 @@
 import {useAuthStore} from "../stores/authStore.js";
 import {useNotificationStore} from "../stores/notificationStore.js";
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import {useNavigate} from "react-router-dom";
 import DefaultSpinner from "../components/DefaultSpinner.jsx";
 import NotificationList from "../components/NotificationList.jsx";
-import {Alert, Typography} from "@material-tailwind/react";
+import {Alert, Button, Typography} from "@material-tailwind/react";
 import NProgress from "nprogress";
+import {FaTrashAlt} from "react-icons/fa";
 
 const Notifications = () => {
     const {userInfo} = useAuthStore();
-    const {notifications, notificationLoading, notificationError, getUserNotifications, readAllNotifications, deleteANotification} = useNotificationStore();
+    const {notifications, notificationLoading, notificationError, getUserNotifications, readAllNotifications, deleteANotification, deleteAllNotifications} = useNotificationStore();
 
     const navigate = useNavigate();
 
@@ -32,32 +33,62 @@ const Notifications = () => {
         }
     }
 
+    const handleDeleteAll = async (e) => {
+        e.preventDefault();
+        try{
+            NProgress.start();
+            await deleteAllNotifications();
+        }catch(err){
+            console.log(err);
+        }finally{
+            NProgress.done();
+        }
+    }
+
     return (
         <main>
-            <section className="flex w-full justify-center items-center">
-                {notificationError && (
-                    <Alert color="red">
-                        {notificationError}
-                    </Alert>
-                )}
-
-                {notificationLoading ? (
+            {notificationLoading ? (
+                <div className="flex w-full justify-center items-center">
                     <DefaultSpinner />
-                ):(
-                    notifications ? (
-                        <div className="w-full flex flex-col justify-center items-center">
-                            <Typography variant="h1">
+                </div>
+            ):(
+                <section className="flex w-full justify-center items-center">
+                    <div className="w-full flex flex-col justify-center items-center">
+                        {notificationError && (
+                            <Alert color="red" className="w-fit">
+                                {notificationError}
+                            </Alert>
+                        )}
+                        <div className="w-full flex justify-center items-center">
+                            <Typography variant="h1" className="text-center">
                                 Notifications
                             </Typography>
-                            <NotificationList notifications={notifications} handleDelete={handleDelete}/>
                         </div>
-                    ):(
-                        <Typography className="text-center">
-                            No notifications
-                        </Typography>
-                    )
-                )}
-            </section>
+                        {notifications.length ? (
+                            <>
+                                <div className="w-full max-w-2xl py-3">
+                                    <Button
+                                        variant="gradient"
+                                        color="red"
+                                        onClick={handleDeleteAll}
+                                        className="flex items-center gap-3 ml-auto"
+                                    >
+                                        <Typography as="span">
+                                            Delete All
+                                        </Typography>
+                                        <FaTrashAlt size={24}/>
+                                    </Button>
+                                </div>
+                                <NotificationList notifications={notifications} handleDelete={handleDelete}/>
+                            </>
+                        ):(
+                            <Typography variant="lead" className="text-center">
+                                No notifications
+                            </Typography>
+                        )}
+                    </div>
+                </section>
+            )}
         </main>
     );
 };
