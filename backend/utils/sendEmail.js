4f9f89ffa5a0916d7df8401ce51c6375a7e5344e
Kevin <pintokevin2002@hotmail.com>
2024-12-10 11:35:34 +0100

    Updating the way emails are sent and adding a function to send emails for notifications

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,27 +1,45 @@
 const nodemailer = require('nodemailer');
 const {parseHtmlToText} = require('./parseHtmlToText');
 
-const sendEmail = async (to, subject, html) => {
-    const transporter = nodemailer.createTransport({
-        host: process.env.MAILER_HOST,
-        port: process.env.MAILER_PORT,
-        secure: process.env.MAILER_SECURE,
-        auth: {
-            user: process.env.MAILER_USER,
-            pass: process.env.MAILER_PASS
-        }
-    });
+const transporter = nodemailer.createTransport({
+    host: process.env.MAILER_HOST,
+    port: process.env.MAILER_PORT,
+    secure: process.env.MAILER_SECURE,
+    auth: {
+        user: process.env.MAILER_USER,
+        pass: process.env.MAILER_PASS
+    }
+});
 
-    const info = await transporter.sendMail({
-        from: `"GamesLock" <${process.env.MAILER_USER}>`,
-        to: process.env.NODE_ENV === 'dev' ? process.env.MAILER_DEV_EMAIL : to,
-        subject: subject,
-        text: parseHtmlToText(html),
-        html: html
-    });
-    console.log("Message sent: %s", info.messageId); // TODO: remove this line
+const sendEmail = async (to, subject, html) => {
+    try{
+        await transporter.sendMail({
+            from: `"GamesLock" <${process.env.MAILER_USER}>`,
+            to: process.env.NODE_ENV === 'dev' ? process.env.MAILER_DEV_EMAIL : to,
+            subject,
+            text: parseHtmlToText(html),
+            html
+        });
+    }catch(err){
+        console.log(err);
+    }
+}
+
+const sendEmailNotification = async (to=[], subject, html) => {
+    try{
+        await transporter.sendMail({
+            from: `"GamesLock" <${process.env.MAILER_USER}>`,
+            bcc: process.env.NODE_ENV === 'dev' ? [...to, process.env.MAILER_DEV_EMAIL] : to,
+            subject,
+            text: parseHtmlToText(html),
+            html
+        });
+    }catch(err){
+        console.log(err);
+    }
 }
 
 module.exports = {
-    sendEmail
+    sendEmail,
+    sendEmailNotification,
 }
