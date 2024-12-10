49a1f7bbfc548b2f310ffded70487f4217a6c879
Kevin <pintokevin2002@hotmail.com>
2024-12-10 18:30:49 +0100

    Updating the NotificationList.jsx

--- frontend/src/components/NotificationList.jsx
+++ frontend/src/components/NotificationList.jsx
@@ -1,18 +1,10 @@
 import Notification from "./Notification.jsx";
 
-const NotificationList = ({notifications}) => {
-    const handleDelete = async (id) => {
-        console.log(id);
-    }
-
-    const handleRead = async (id) => {
-        console.log(id);
-    }
-
+const NotificationList = ({notifications, handleDelete}) => {
     return (
         <div className="w-full max-w-2xl flex flex-col gap-3">
             {notifications.map((notification, index) => (
-                <Notification key={index} notification={notification} handleDelete={handleDelete} handleRead={handleRead} />
+                <Notification key={index} notification={notification} handleDelete={handleDelete}/>
             ))}
         </div>
     );
