8c37d4f6ed730a91a4da2691b1312f876ad54058
Kevin <pintokevin2002@hotmail.com>
2024-12-11 20:41:31 +0100

    Making sure every text is similar

--- frontend/src/components/CountDown.jsx
+++ frontend/src/components/CountDown.jsx
@@ -4,12 +4,13 @@ import {Typography} from "@material-tailwind/react";
 
 const CountDown = ({duration=900, isDark=true}) => {
     const [key, setKey] = useState(0);
+    const className = `text-center text-primary-${isDark ? '900' : '0'}`;
 
     const renderTime = ({ remainingTime }) => {
         if(remainingTime === 0){
             return (
                 <div className="timer">
-                    <Typography>
+                    <Typography variant="lead" className={className}>
                         Time's up!
                     </Typography>
                 </div>
@@ -24,17 +25,17 @@ const CountDown = ({duration=900, isDark=true}) => {
         return (
             <div className="timer flex flex-col justify-center items-center">
                 <div>
-                    <Typography className={`text-center text-primary-${isDark ? '900' : '0'}`}>
+                    <Typography className={className}>
                         Remaining
                     </Typography>
                 </div>
                 <div role="timer" aria-live="assertive">
-                    <Typography variant="lead" className={`text-center text-primary-${isDark ? '900' : '0'}`}>
+                    <Typography variant="lead" className={className}>
                         {text}
                     </Typography>
                 </div>
                 <div>
-                    <Typography className={`text-center text-primary-${isDark ? '900' : '0'}`}>
+                    <Typography className={className}>
                         time
                     </Typography>
                 </div>
