e3b5ce5f278b964ae0feb86a84e442b553c82f75
Kevin <pintokevin2002@hotmail.com>
2024-12-12 22:37:44 +0100

    Updating the way we handle the deletion of the user pfp, so it's synchronous and will try maximum 5 time

--- backend/utils/deleteProfilePicture.js
+++ backend/utils/deleteProfilePicture.js
@@ -4,14 +4,24 @@ const RootPath = require('../rootPath');
 
 const deleteProfilePicture = (imageName) => {
     const imagePath = `${RootPath}/uploads/${imageName}`;
+    const maxIterations = 5;
 
-    fs.access(imagePath, fs.constants.F_OK, (err) => {
-        if(!err && !checkIfPfpIsDefault(imageName)){
-            fs.unlink(imagePath, (error) => {
-                if(error) console.error(error);
-            });
-        }
-    });
+    if(!checkIfPfpIsDefault(imageName)){
+        let exist = false;
+        let iteration = 0;
+        do{
+            exist = false;
+            iteration++;
+            if(fs.existsSync(imagePath)){
+                exist = true;
+                try{
+                    fs.unlinkSync(imagePath);
+                }catch(error){
+                    console.error(error);
+                }
+            }
+        }while(exist || iteration < maxIterations);
+    }
 }
 
 module.exports = deleteProfilePicture;
