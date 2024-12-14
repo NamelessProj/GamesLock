d3616cd8cdba633805315598f3d5c5e7f4ce13a9
Kevin Da Silva Pinto <pintokevin2002@hotmail.com>
2024-12-14 06:31:38 +0100

    Update createOTP.js
    Putting the max number of digits to the number of digits Math.random generate

--- backend/utils/createOTP.js
+++ backend/utils/createOTP.js
@@ -1,5 +1,5 @@
 const createOTP = (numOfDigits=6) => {
-    const digits = numOfDigits > 6 ? 5 : numOfDigits - 1; // Limit the number of digits to 6. And soustract 1
+    const digits = numOfDigits > 17 ? 16 : numOfDigits - 1; // Limit the number of digits to 17, since it's the number of digits Math.random can generate. And soustract 1
     const addNum = Math.pow(10, digits); // Calculate the minimum number to be added to the random number. If numOfDigits = 6 then addNum = 100000
     const multiplyNum = addNum * 9; // Calculate the maximum number to be multiplied to the random number. If numOfDigits = 6 then multiplyNum = 900000
     return Math.floor(addNum + Math.random() * multiplyNum).toString(); // Generate a random number with a fixed number of digits and return it as a string
