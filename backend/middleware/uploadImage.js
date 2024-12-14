14cec49ebf08bf5d04a6e058a6bfefb0f081405c
Kevin <pintokevin2002@hotmail.com>
2024-12-14 14:27:55 +0100

    Updating the way we upload images. Adding a function to download profile picture and one for other picture

--- backend/middleware/uploadImage.js
+++ backend/middleware/uploadImage.js
@@ -1,37 +1,53 @@
 const multer = require('multer');
 const path = require('path');
 const removeDiacritics = require('../utils/removeDiacritics');
+const RootPath = require('../rootPath');
 
-const upload = multer({
+const filename = (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    let fileName = path.basename(file.originalname, ext).toLowerCase();
+    fileName = fileName.replace(/['"]*/g, '');
+    fileName = removeDiacritics(fileName);
+    fileName = fileName.split(' ').join('-');
+    fileName = fileName.replace(/-{2,}/g, '-');
+    const finalFileName = `${Date.now()}-${Math.round(Math.random() * 1E9)}-${fileName}${ext}`;
+    cb(null, finalFileName);
+}
+
+const fileFilter = (req, file, cb) => {
+    const allowedTypes = /jpeg|jpg|png/;
+    const allowedMimeTypes = ['image/jpeg', 'image/jpg', 'image/png'];
+    const mimeType = allowedTypes.test(file.mimetype);
+    const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+
+    if(allowedMimeTypes.includes(file.mimetype) && mimeType && extname){
+        cb(null, true);
+    }else{
+        cb(new Error('Only .jpeg, jpg or .png files are allowed'));
+    }
+}
+
+const uploadPfp = multer({
     storage: multer.diskStorage({
         destination: (req, file, cb) => {
-            cb(null, './uploads');
+            cb(null, `${RootPath}/uploads/user`);
         },
-        filename: (req, file, cb) => {
-            const ext = path.extname(file.originalname);
-            let fileName = path.basename(file.originalname, ext).toLowerCase();
-            fileName = fileName.replace(/['"]*/g, '');
-            fileName = removeDiacritics(fileName);
-            fileName = fileName.split(' ').join('-');
-            fileName = fileName.replace(/-{2,}/g, '-');
-            const finalFileName = `${Date.now()}-${Math.round(Math.random() * 1E9)}-${fileName}${ext}`;
-            cb(null, finalFileName);
-        }
+        filename
     }),
-    fileFilter: (req, file, cb) => {
-        const allowedTypes = /jpeg|jpg|png/;
-        const allowedMimeTypes = ['image/jpeg', 'image/jpg', 'image/png'];
-        const mimeType = allowedTypes.test(file.mimetype);
-        const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+    fileFilter
+});
 
-        if(allowedMimeTypes.includes(file.mimetype) && mimeType && extname){
-            cb(null, true);
-        }else{
-            cb(new Error('Only .jpeg, jpg or .png files are allowed'));
-        }
-    }
+const uploadImage = multer({
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, `${RootPath}/uploads`);
+        },
+        filename
+    }),
+    fileFilter
 });
 
 module.exports = {
-    upload,
+    uploadPfp,
+    uploadImage,
 };
