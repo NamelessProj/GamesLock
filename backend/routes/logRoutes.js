84b96ce421aaf9efde7fbfda686a5b81c7854fb3
Kevin <pintokevin2002@hotmail.com>
2024-12-14 15:04:17 +0100

    Adding the route to get all the logs of a user

--- backend/routes/logRoutes.js
+++ backend/routes/logRoutes.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const logController = require('../controllers/logController');
 const {protect} = require('../middleware/authMiddleware');
 
+// @route Log route (GET)
+// @desc Route to get all the logs of a user
+// @access Private
+router.route('/').get(protect, logController.getLogs);
+
 // @route Log route (DELETE)
 // @desc Route to delete a log with an id
 // @access Private
