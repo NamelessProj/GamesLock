3b677743cea6f560ebd9979574379ba6f0a7eb91
Kevin <pintokevin2002@hotmail.com>
2024-12-14 14:34:26 +0100

    Adding a "/"

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -343,7 +343,7 @@ const removeProfilePicture = asyncHandler(async (req, res) => {
     user.profileImage = 'default.jpg';
 
     // Getting the average color of the image
-    const color = await getAverageColorOfImage(`./uploads/user${user.profileImage}`);
+    const color = await getAverageColorOfImage(`./uploads/user/${user.profileImage}`);
     user.profileColor.hex = color.hex;
     user.profileColor.rgb = color.rgb;
     user.profileColor.isDark = color.isDark;
