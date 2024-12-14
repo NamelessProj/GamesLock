d7953a26d4fdcc6bc108522bc29ac46aa56f605a
Kevin Da Silva Pinto <pintokevin2002@hotmail.com>
2024-12-14 06:14:45 +0100

    Update createOTP.js
    Improve performance of the algorithme

--- backend/utils/createOTP.js
+++ backend/utils/createOTP.js
@@ -1,7 +1,7 @@
 const createOTP = (numOfDigits=6) => {
     const digits = numOfDigits > 6 ? 5 : numOfDigits - 1; // Limit the number of digits to 6. And soustract 1
     const addNum = Math.pow(10, digits); // Calculate the minimum number to be added to the random number. If numOfDigits = 6 then addNum = 100000
-    const multiplyNum = Math.pow(10, digits) * 9; // Calculate the maximum number to be multiplied to the random number. If numOfDigits = 6 then multiplyNum = 900000
+    const multiplyNum = addNum * 9; // Calculate the maximum number to be multiplied to the random number. If numOfDigits = 6 then multiplyNum = 900000
     return Math.floor(addNum + Math.random() * multiplyNum).toString(); // Generate a random number with a fixed number of digits and return it as a string
 }
 
