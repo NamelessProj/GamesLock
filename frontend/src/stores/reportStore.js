4fb92dab435d9e7ac55fa50faf8ce7ce6a941c55
Kevin <pintokevin2002@hotmail.com>
2024-12-15 20:29:22 +0100

    Adding the store for the reports

---
+++ frontend/src/stores/reportStore.js
@@ -0,0 +1,33 @@
+import {create} from "zustand";
+import axios from "axios";
+
+export const useReportStore = create((set) => ({
+    reports: [],
+    reportLoading: false,
+    reportError: null,
+
+    getReports: async () => {
+        set(() => ({reportLoading: true, reportError: null}));
+        try{
+            const response = await axios.get(`${import.meta.env.VITE_API_URL}report`, {
+                method: 'get',
+                withCredentials: true,
+            });
+            set(() => ({reports: response.data.reports, reportLoading: false}));
+        }catch(error){
+            set({reportError: error.message, reportLoading: false});
+        }
+    },
+
+    addReport: async (id) => {
+        set(() => ({reportError: null}));
+        try{
+            await axios.post(`${import.meta.env.VITE_API_URL}report/${id}`, null,{
+                method: 'post',
+                withCredentials: true,
+            });
+        }catch(error){
+            set({reportError: error.message});
+        }
+    },
+}));
