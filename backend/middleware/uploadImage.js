5fd06e8d217f4701c539d3f784d52421a8ad634f
Kevin <pintokevin2002@hotmail.com>
2025-01-15 14:51:42 +0100

    Updating the way we handle the upload of images

--- backend/middleware/uploadImage.js
+++ backend/middleware/uploadImage.js
@@ -1,18 +1,5 @@
 const multer = require('multer');
 const path = require('path');
-const removeDiacritics = require('../utils/removeDiacritics');
-const RootPath = require('../rootPath');
-
-const filename = (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    let fileName = path.basename(file.originalname, ext).toLowerCase();
-    fileName = fileName.replace(/['"]*/g, '');
-    fileName = removeDiacritics(fileName);
-    fileName = fileName.split(' ').join('-');
-    fileName = fileName.replace(/-{2,}/g, '-');
-    const finalFileName = `${Date.now()}-${Math.round(Math.random() * 1E9)}-${fileName}${ext}`;
-    cb(null, finalFileName);
-}
 
 const fileFilter = (req, file, cb) => {
     const allowedTypes = /jpeg|jpg|png/;
@@ -27,27 +14,9 @@ const fileFilter = (req, file, cb) => {
     }
 }
 
-const uploadPfp = multer({
-    storage: multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, `${RootPath}/uploads/user`);
-        },
-        filename
-    }),
-    fileFilter
-});
-
 const uploadImage = multer({
-    storage: multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, `${RootPath}/uploads`);
-        },
-        filename
-    }),
+    storage: multer.memoryStorage(),
     fileFilter
 });
 
-module.exports = {
-    uploadPfp,
-    uploadImage,
-};
+module.exports = {uploadImage};
