5739a375d48586426c57422ff67ada80e7b1a87e
Kevin <pintokevin2002@hotmail.com>
2025-01-16 15:02:29 +0100

    Adding more control when uploading images

--- backend/utils/uploadImage.js
+++ backend/utils/uploadImage.js
@@ -1,18 +1,39 @@
 const sharp = require('sharp');
 const RootPath = require('../rootPath');
 
-const uploadImage = async (file, folder="") => {
-    const path= folder ? `${RootPath}/uploads/${folder}` : `${RootPath}/uploads`;
-    const ext = "webp";
-    const filename = `${Date.now()}-${Math.round(Math.random() * 1E9)}.${ext}`;
+const uploadImage = async (file, options={folder: '', filename: '', format: 'webp', width: 500, height: 500, fit: 'inside', quality: 90}) => {
+    const path= options.folder !== '' ? `${RootPath}/uploads/${options.folder}` : `${RootPath}/uploads`;
+
+    let ext = 'webp';
+    let filename = options.filename !== '' ? `${options.filename}.` : `${Date.now()}-${Math.round(Math.random() * 1E9)}.`;
     let success = false;
 
+    const image = sharp(file.buffer).resize({width: options.width, height: options.height, fit: options.fit});
+
+    switch(options.format){
+        case 'jpg':
+        case 'jpeg':
+            ext = 'jpeg';
+            image.toFormat(ext);
+            image.jpeg({quality: options.quality});
+            break;
+
+        case 'png':
+            ext = 'png';
+            image.toFormat(ext);
+            image.png({quality: options.quality});
+            break;
+
+        case 'webp':
+        default:
+            image.toFormat(ext);
+            image.webp({quality: options.quality});
+            break;
+    }
+    filename += ext;
+
     try{
-        await sharp(file.buffer)
-            .resize({width: 500, height: 500, fit: 'inside'})
-            .toFormat(ext)
-            .webp({quality: 90})
-            .toFile(`${path}/${filename}`);
+        await image.toFile(`${path}/${filename}`);
         success = true;
     }catch(error){
         console.error(error);
