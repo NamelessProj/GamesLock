a6d8421bdc8b32baf6d5ff3170b5c5d3a47ec022
Kevin <pintokevin2002@hotmail.com>
2025-01-23 17:10:37 +0100

    Using the new name for the cookie used by jwt

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
+const JWT_NAME = require('../JWT_NAME');
 
 const generateToken = (res, userId) => {
     const token = jwt.sign({userId}, process.env.JWT_SECRET, {
         expiresIn: '30d'
     });
 
-    res.cookie('jwt', token, {
+    res.cookie(JWT_NAME, token, {
         httpOnly: true,
         secure: process.env.NODE_ENV  !== 'dev',
         sameSite: 'strict',
