fff3a57688080528d61295f9507efd290a9fb507
Kevin <pintokevin2002@hotmail.com>
2025-01-26 15:26:57 +0100

    Adding a reportSuccess state

--- frontend/src/stores/reportStore.js
+++ frontend/src/stores/reportStore.js
@@ -5,6 +5,7 @@ export const useReportStore = create((set) => ({
     reports: [],
     reportLoading: false,
     reportError: null,
+    reportSuccess: false,
 
     getReports: async () => {
         set(() => ({reportLoading: true, reportError: null}));
@@ -20,14 +21,15 @@ export const useReportStore = create((set) => ({
     },
 
     addReport: async (id) => {
-        set(() => ({reportError: null}));
+        set(() => ({reportError: null, reportSuccess: false}));
         try{
             await api.post(`report/${id}`, null,{
                 method: 'post',
                 withCredentials: true,
             });
+            set(() => ({reportSuccess: true}));
         }catch(error){
-            set({reportError: error.response.data.message || error.message});
+            set({reportError: error.response.data.message || error.message, reportSuccess: false});
         }
     },
 }));
