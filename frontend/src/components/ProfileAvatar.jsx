01e1ba9c8944c4196776a16d2a911e0f8b60328c
Kevin <pintokevin2002@hotmail.com>
2025-01-31 17:41:10 +0100

    Adding an explosive breaking end

--- frontend/src/components/ProfileAvatar.jsx
+++ frontend/src/components/ProfileAvatar.jsx
@@ -1,13 +1,29 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import ConfettiExplosion from "react-confetti-explosion";
 import {Avatar, Typography} from "@material-tailwind/react";
 import {getUserPfp} from "../utils/getUserPfp.js";
+import {extractColors} from "extract-colors";
 
 const ProfileAvatar = ({user, t, brokeStartAt=100, breakingSteps=10}) => {
     const [pfpClickTotal, setPfpClickTotal] = useState(0);
     const [pfpStep, setPfpStep] = useState(0);
     const [pfpIsExploded, setPfpIsExploded] = useState(false);
     const [pfpIsBroken, setPfpIsBroken] = useState(false);
+    const [colorsArray, setColorsArray] = useState(['purple', 'black', 'gray']);
+    const [checkColors, setCheckColors] = useState(false);
+
+    const userPfp = getUserPfp(user);
+
+    useEffect(() => {
+        if(checkColors){
+            extractColors(userPfp).then(
+                (colors) => {
+                    setColorsArray(colors.map(color => color.hex));
+                }).catch((error) => {
+                console.error(error);
+            });
+        }
+    }, [checkColors]);
 
     const brokenImageArray = Array.from({length: breakingSteps});
 
@@ -18,6 +34,7 @@ const ProfileAvatar = ({user, t, brokeStartAt=100, breakingSteps=10}) => {
         if(pfpClickTotal > 9 && pfpClickTotal < 25){
             setPfpStep(1);
         }else if(pfpClickTotal > 30 && pfpClickTotal < 40){
+            setCheckColors(true);
             setPfpStep(2);
         }else if(pfpClickTotal > 75 && pfpClickTotal < 85){
             setPfpStep(3);
@@ -30,8 +47,8 @@ const ProfileAvatar = ({user, t, brokeStartAt=100, breakingSteps=10}) => {
     return (
         <div className="w-full flex justify-center items-center flex-col relative">
             {pfpIsExploded && <ConfettiExplosion />}
-            {pfpIsBroken && <ConfettiExplosion colors={['green', 'light-green', 'white']} particleCount={50} />}
-            <Avatar src={pfpIsBroken ? '/transparent.png' : getUserPfp(user)} className={`${pfpIsBroken ? "" : "cursor-pointer"} ${pfpIsExploded ? "pfp-is-egg" : ""}`} onClick={handleClickOnPfp} loading="lazy" variant="circular" alt={user?.username} size="xxl"/>
+            {pfpIsBroken && <ConfettiExplosion colors={colorsArray} particleCount={50} />}
+            <Avatar src={pfpIsBroken ? '/transparent.png' : userPfp} className={`${pfpIsBroken ? "" : "cursor-pointer"} ${pfpIsExploded ? "pfp-is-egg" : ""}`} onClick={handleClickOnPfp} loading="lazy" variant="circular" alt={user?.username} size="xxl"/>
             {pfpStep !== 0 && (
                 <div className="bg-primary-0 my-2 p-3 rounded-md absolute -bottom-[100%] transform -translate-y-[80%] z-30">
                     <Typography className="text-center text-balance">
