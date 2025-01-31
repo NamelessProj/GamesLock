7cfe0a186b81985f76b5b5161adc88358f0f38c6
Kevin <pintokevin2002@hotmail.com>
2025-01-31 17:13:07 +0100

    Adding a little easter egg to break the pfp

--- frontend/src/components/ProfileAvatar.jsx
+++ frontend/src/components/ProfileAvatar.jsx
@@ -3,10 +3,13 @@ import ConfettiExplosion from "react-confetti-explosion";
 import {Avatar, Typography} from "@material-tailwind/react";
 import {getUserPfp} from "../utils/getUserPfp.js";
 
-const ProfileAvatar = ({user, t}) => {
+const ProfileAvatar = ({user, t, brokeStartAt=100, breakingSteps=10}) => {
     const [pfpClickTotal, setPfpClickTotal] = useState(0);
     const [pfpStep, setPfpStep] = useState(0);
     const [pfpIsExploded, setPfpIsExploded] = useState(false);
+    const [pfpIsBroken, setPfpIsBroken] = useState(false);
+
+    const brokenImageArray = Array.from({length: breakingSteps});
 
     const handleClickOnPfp = (e) => {
         e.preventDefault();
@@ -16,21 +19,31 @@ const ProfileAvatar = ({user, t}) => {
             setPfpStep(1);
         }else if(pfpClickTotal > 30 && pfpClickTotal < 40){
             setPfpStep(2);
-        }else if(pfpClickTotal > 75){
+        }else if(pfpClickTotal > 75 && pfpClickTotal < 85){
             setPfpStep(3);
             setPfpIsExploded(true);
+        }else if(pfpClickTotal >= brokeStartAt + (breakingSteps - 2) * 10){
+            setPfpIsBroken(true);
         }
     }
 
     return (
         <div className="w-full flex justify-center items-center flex-col relative">
             {pfpIsExploded && <ConfettiExplosion />}
-            <Avatar src={getUserPfp(user)} className={`cursor-pointer ${pfpIsExploded ? "pfp-is-egg" : ""}`} onClick={handleClickOnPfp} loading="lazy" variant="circular" alt={user?.username} size="xxl"/>
+            {pfpIsBroken && <ConfettiExplosion colors={['green', 'light-green', 'white']} particleCount={50} />}
+            <Avatar src={pfpIsBroken ? '/transparent.png' : getUserPfp(user)} className={`${pfpIsBroken ? "" : "cursor-pointer"} ${pfpIsExploded ? "pfp-is-egg" : ""}`} onClick={handleClickOnPfp} loading="lazy" variant="circular" alt={user?.username} size="xxl"/>
             {pfpStep !== 0 && (
                 <div className="bg-primary-0 my-2 p-3 rounded-md absolute -bottom-[100%] transform -translate-y-[80%] z-30">
                     <Typography className="text-center text-balance">
                         {t(`profile.easteregg.${pfpStep}`)}
                     </Typography>
+                </div>
+            )}
+            {(pfpClickTotal >= brokeStartAt && !pfpIsBroken) && (
+                <div className="broken-pfp-container absolute inset-0 flex justify-center items-center select-none pointer-events-none">
+                    {brokenImageArray.map((_, index) => (
+                        <img key={index} src={`/minecraft/destroy_stage_${index}.png`} className={`broken-pfp h-full rounded-full select-none pointer-events-none ${pfpClickTotal >= brokeStartAt + index * 10 ? "broken-pfp-block" : "hidden"}`} />
+                    ))}
                 </div>
             )}
         </div>
