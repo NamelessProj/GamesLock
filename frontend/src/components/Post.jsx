5f56b904cb2d1899d44ab234596f28d6e44f408d
Kevin <pintokevin2002@hotmail.com>
2025-02-06 16:03:10 +0100

    Adding custom elements for the Markdown parser

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -145,9 +145,19 @@ const Post = ({post, handleShareDialog=null, handleDialog=null, setPost=null, lo
                     )}
                     <div className="post-content-container text-primary-900">
                         {post.isFromUser ? (
-                            <Markdown>
-                                {post.text}
-                            </Markdown>
+                            <Markdown
+                                children={post.text}
+                                components={{
+                                    h1: ({node, ...props}) => <Typography variant="h1" as="p" {...props} />,
+                                    h2: ({node, ...props}) => <Typography variant="h2" as="p" {...props} />,
+                                    h3: ({node, ...props}) => <Typography variant="h3" as="p" {...props} />,
+                                    h4: ({node, ...props}) => <Typography variant="h4" as="p" {...props} />,
+                                    h5: ({node, ...props}) => <Typography variant="h5" as="p" {...props} />,
+                                    h6: ({node, ...props}) => <Typography variant="h6" as="p" {...props} />,
+                                    p: ({node, ...props}) => <Typography {...props} />,
+                                    a: ({node, ...props}) => <a {...props} className="font-bold text-primary-400 hover:underline" target="_blank" rel="noreferrer" />,
+                                }}
+                            />
                         ):(
                             <Typography className="w-full text-base">
                                 {t(`achievement.titles.${post.text}`)}
