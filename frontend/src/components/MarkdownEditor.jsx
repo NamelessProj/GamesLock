4371881d399aecb18dfd5b296b60ae637dea04af
Kevin <pintokevin2002@hotmail.com>
2025-02-06 16:34:20 +0100

    Adding a placeholder for the MarkdownEditor.jsx

--- frontend/src/components/MarkdownEditor.jsx
+++ frontend/src/components/MarkdownEditor.jsx
@@ -1,12 +1,16 @@
 import MDEditor from '@uiw/react-md-editor/nohighlight';
 import rehypeSanitize from "rehype-sanitize";
 
-const MarkdownEditor = ({value, setValue, colorMode="dark"}) => {
+const MarkdownEditor = ({value, setValue, placeholder="", colorMode="dark"}) => {
     return (
         <div className="md-editor-container" data-color-mode={colorMode}>
             <MDEditor
                 value={value}
                 onChange={setValue}
+                highlightEnable={false}
+                textareaProps={{
+                    placeholder: placeholder,
+                }}
                 previewOptions={{
                     rehypePlugins: [[rehypeSanitize]]
                 }}
