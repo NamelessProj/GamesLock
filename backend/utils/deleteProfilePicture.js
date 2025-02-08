70f9c57db1b7d80355f28bca0a189029e1d84764
Kevin <pintokevin2002@hotmail.com>
2025-02-08 13:31:11 +0100

    Adding a function to delete images

--- backend/utils/deleteProfilePicture.js
+++ backend/utils/deleteProfilePicture.js
@@ -3,7 +3,7 @@ const checkIfPfpIsDefault = require('./checkIfPfpIsDefault');
 const RootPath = require('../rootPath');
 const {sendEmail} = require('./sendEmail');
 
-const deleteProfilePicture = async (imageName, maxIterations=5) => {
+const deleteProfilePicture = async (imageName, maxIteration=5) => {
     const imagePath = `${RootPath}/uploads/user/${imageName}`;
 
     if(!checkIfPfpIsDefault(imageName)){
@@ -20,11 +20,37 @@ const deleteProfilePicture = async (imageName, maxIterations=5) => {
                     console.error(error);
                 }
             }
-            if(iteration >= maxIterations) break;
+            if(iteration >= maxIteration) break;
         }while(exist);
 
-        if(exist) await sendEmail(process.env.ADMIN_EMAIL, 'Error deleting an image', `<p>Error deleting profile picture:<br/><b>${imageName}</b></p>`);
+        if(exist) await sendEmail(process.env.ADMIN_EMAIL, 'Error deleting a profile picture', `<p>Error deleting profile picture:<br/><b>${imageName}</b></p>`);
     }
 }
 
-module.exports = deleteProfilePicture;
+const deleteImage = async (imageName, folderName="", maxIteration=5) => {
+    const imagePath = `${RootPath}/uploads/${folderName !== "" ? `${folderName}/` : ""}${imageName}`;
+
+    let exist = false;
+    let iteration = 0;
+
+    do{
+        exist = false;
+        iteration++;
+        if(fs.existsSync(imagePath)){
+            exist = true;
+            try{
+                fs.unlinkSync(imagePath);
+            }catch(error){
+                console.error(error);
+            }
+        }
+        if(iteration >= maxIteration) break;
+    }while(exist);
+
+    if(exist) await sendEmail(process.env.ADMIN_EMAIL, 'Error deleting an image', `<p>Error deleting the image:<br/><b>${imageName}</b></p>`);
+}
+
+module.exports = {
+    deleteProfilePicture,
+    deleteImage,
+};
