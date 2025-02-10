140d80c0fc7d5ff9da0488445ecd90b52bb9d8e2
Kevin <pintokevin2002@hotmail.com>
2025-02-10 13:16:51 +0100

    Using the "followUser" context to display the name of the user we're trying to follow/unfollow

--- frontend/src/utils/HandleToast.jsx
+++ frontend/src/utils/HandleToast.jsx
@@ -1,15 +1,18 @@
-import {useEffect} from "react";
+import {useContext, useEffect} from "react";
 import {toast} from "react-toastify";
 import {useTranslation} from "react-i18next";
 import {useReportStore} from "../stores/reportStore.js";
 import {useUserStore} from "../stores/userStore.js";
 import {useFollowStore} from "../stores/followStore.js";
+import DataContext from "../context/DataContext.jsx";
 
 const HandleToast = ({children}) => {
     const {t} = useTranslation();
     const {reportError, reportSuccess} = useReportStore();
     const {userError, userEditSuccess} = useUserStore();
     const {addFollowSuccess, deleteFollowSuccess} = useFollowStore();
+
+    const {followUser} = useContext(DataContext);
 
     // REPORTS
     useEffect(() => {
@@ -29,10 +32,10 @@ const HandleToast = ({children}) => {
 
     // FOLLOW
     useEffect(() => {
-        if(addFollowSuccess) toast(t("profile.followUser.add"), {type: "success"});
+        if(addFollowSuccess) toast(t("profile.followUser.add", {user: followUser.username}), {type: "success"});
     }, [addFollowSuccess]);
     useEffect(() => {
-        if(deleteFollowSuccess) toast(t("profile.followUser.remove"), {type: "success"});
+        if(deleteFollowSuccess) toast(t("profile.followUser.remove", {user: followUser.username}), {type: "success"});
     }, [deleteFollowSuccess]);
 
     return children;
