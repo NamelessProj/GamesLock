00a7b43809fb62c70f933959446a7f4ab50f5c8c
Pinto <pintokevin2002@hotmail.com>
2025-02-11 12:11:02 +0100

    Making all checkInput into one file

---
+++ frontend/src/utils/validateInput.js
@@ -0,0 +1,11 @@
+const checkEmail = (email) => {
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(email);
+}
+
+const checkPassword = (password) => {
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+    return passwordRegex.test(password);
+}
+
+export default {checkEmail, checkPassword};
