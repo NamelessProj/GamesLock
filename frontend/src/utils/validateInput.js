cb1567c6d95eb7c5facb87eb0c3f50cb3bbe3cd7
Kevin <pintokevin2002@hotmail.com>
2025-02-11 19:49:54 +0100

    Updating the way we export validate functions

--- frontend/src/utils/validateInput.js
+++ frontend/src/utils/validateInput.js
@@ -1,11 +1,9 @@
-const checkEmail = (email) => {
+export const validateEmail = (email) => {
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return emailRegex.test(email);
 }
 
-const checkPassword = (password) => {
+export const validatePassword = (password) => {
     const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
     return passwordRegex.test(password);
 }
-
-export default {checkEmail, checkPassword};
