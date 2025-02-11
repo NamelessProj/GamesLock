a861d0b787e5d6b90b04a222915a95d64cd3c38b
Pinto <pintokevin2002@hotmail.com>
2025-02-11 09:49:02 +0100

    Removing white spaces

--- frontend/src/pages/EditProfile.jsx
+++ frontend/src/pages/EditProfile.jsx
@@ -66,7 +66,7 @@ const EditProfile = () => {
                         </>
                     )}
                 </>
-            ) : (
+            ):(
                 <Navigate to="/login"/>
             )}
         </main>
