022f26020f17d19c0cf2ff6e0d997452386985f9
Kevin <pintokevin2002@hotmail.com>
2025-02-11 20:34:01 +0100

    Updating the way we handle to delete file, by removing duplicate code

--- backend/utils/deleteImage.js
+++ backend/utils/deleteImage.js
@@ -3,51 +3,42 @@ const checkIfPfpIsDefault = require('./checkIfPfpIsDefault');
 const RootPath = require('../rootPath');
 const {sendEmail} = require('./sendEmail');
 
-const deleteProfilePicture = async (imageName, maxIteration=5) => {
-    const imagePath = `${RootPath}/uploads/user/${imageName}`;
-
-    if(!checkIfPfpIsDefault(imageName)){
-        let exist = false;
-        let iteration = 0;
-        do{
-            exist = false;
-            iteration++;
-            if(fs.existsSync(imagePath)){
-                exist = true;
-                try{
-                    fs.unlinkSync(imagePath);
-                }catch(error){
-                    console.error(error);
-                }
-            }
-            if(iteration >= maxIteration) break;
-        }while(exist);
-
-        if(exist) await sendEmail(process.env.ADMIN_EMAIL, 'Error deleting a profile picture', `<p>Error deleting profile picture:<br/><b>${imageName}</b></p>`);
-    }
-}
-
-const deleteImage = async (imageName, folderName="", maxIteration=5) => {
-    const imagePath = `${RootPath}/uploads/${folderName !== "" ? `${folderName}/` : ""}${imageName}`;
-
+const deleteFile = (path, maxIteration=5) => {
     let exist = false;
     let iteration = 0;
 
     do{
         exist = false;
         iteration++;
-        if(fs.existsSync(imagePath)){
+
+        if(fs.existsSync(path)){
             exist = true;
+
             try{
-                fs.unlinkSync(imagePath);
+                fs.unlinkSync(path);
             }catch(error){
                 console.error(error);
             }
         }
+
         if(iteration >= maxIteration) break;
     }while(exist);
 
-    if(exist) await sendEmail(process.env.ADMIN_EMAIL, 'Error deleting an image', `<p>Error deleting the image:<br/><b>${imageName}</b></p>`);
+    return exist;
+}
+
+const deleteProfilePicture = async (imageName, maxIteration=5) => {
+    const path = `${RootPath}/uploads/user/${imageName}`;
+
+    if(!checkIfPfpIsDefault(imageName) && deleteFile(path, maxIteration)){
+        await sendEmail(process.env.ADMIN_EMAIL, 'Error deleting a profile picture', `<p>Error deleting profile picture:<br/><b>${imageName}</b></p>`);
+    }
+}
+
+const deleteImage = async (imageName, folderName="", maxIteration=5) => {
+    const path = `${RootPath}/uploads/${folderName !== "" ? `${folderName}/` : ""}${imageName}`;
+
+    if(deleteFile(path, maxIteration)) await sendEmail(process.env.ADMIN_EMAIL, 'Error deleting an image', `<p>Error deleting the image:<br/><b>${imageName}</b></p>`);
 }
 
 module.exports = {
