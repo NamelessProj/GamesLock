856b6b14ef05e079144771b30f66b6d00ea56a32
Kevin <pintokevin2002@hotmail.com>
2025-02-14 23:22:24 +0100

    Adding spaces

--- frontend/src/components/NotificationList.jsx
+++ frontend/src/components/NotificationList.jsx
@@ -4,7 +4,7 @@ const NotificationList = ({notifications, handleDelete}) => {
     return (
         <div className="w-full max-w-2xl flex flex-col gap-3">
             {notifications.map((notification, index) => (
-                <Notification key={index} notification={notification} handleDelete={handleDelete}/>
+                <Notification key={index} notification={notification} handleDelete={handleDelete} />
             ))}
         </div>
     );
